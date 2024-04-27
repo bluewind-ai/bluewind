2d4c8827a52a287ece9460354cb4f7d48c82d750
Merwane Hamadi <merwanehamadi@gmail.com>
2024-04-27 18:01:14 +0200

    add vercel analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,4 @@
 import { Head, Html, Main, NextScript } from 'next/document';
-import { Analytics } from '@vercel/analytics/react';
-
 export default function Document() {
   return (
     <Html lang="en" className="h-full" data-theme="boxyhq">
@@ -8,7 +6,6 @@ export default function Document() {
       <body className="h-full">
         <Main />
         <NextScript />
-        <Analytics />
       </body>
     </Html>
   );
---
+++ pages/layout.tsx
@@ -0,0 +1,19 @@
+import { Analytics } from '@vercel/analytics/react';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
