0104436b69b3de67057d5d212c12a279056bfaa0
Merwane Hamadi <merwanehamadi@gmail.com>
2024-05-08 14:25:12 +0200

    add automation for salutations and update stats

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
 
-const Community = () => {
+const FirstIframe = () => {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-screen">
       <iframe
         className="w-full h-full"
         frameBorder="0"
         allowFullScreen
         src="https://us.posthog.com/embedded/DvBEoYZepCrIJiB0Q-1_MaVE8Bk2aQ"
       ></iframe>
+    </div>
+  );
+};
+
+const SecondIframe = () => {
+  return (
+    <div className="w-full h-screen">
+      <iframe
+        className="w-full h-full"
+        frameBorder="0"
+        allowFullScreen
+        allow="fullscreen"
+        title="Embedded Content"
+        src="https://us.posthog.com/embedded/gxjKcMCqwCoFGso_q-llXb376dvKDw"
+      ></iframe>
+    </div>
+  );
+};
+
+const Community = () => {
+  return (
+    <div>
+      <FirstIframe />
+      <SecondIframe />
     </div>
   );
 };
