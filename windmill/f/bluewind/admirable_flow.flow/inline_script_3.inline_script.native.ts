428869c5563126da13fe39449884db9b775ddd55
Merwane Hamadi <merwanehamadi@gmail.com>
2024-05-24 23:58:56 +0200

    change workspace from merwanehamadi to bluewind

--- windmill/f/bluewind/admirable_flow.flow/inline_script_3.inline_script.native.ts
+++ windmill/f/bluewind/admirable_flow.flow/inline_script_3.inline_script.native.ts
@@ -1,25 +1,25 @@
 export async function main(all_attempts: Array) {
-  const correct_api_key = all_attempts[all_attempts.length - 1].leadmagic_api_key
+  const correct_api_key =
+    all_attempts[all_attempts.length - 1].leadmagic_api_key;
   const data = {
-    path: "u/merwanehamadi/leadmagic",
+    path: 'u/bluewind/leadmagic',
     value: correct_api_key,
-    description: "",
-    resource_type: "leadmagic"
-  }
+    description: '',
+    resource_type: 'leadmagic',
+  };
 
   const url = `${BASE_INTERNAL_URL}/api/w/${WM_WORKSPACE}/resources/create`;
 
   const response = await fetch(url, {
     method: 'POST',
     headers: {
-      'Authorization': `Bearer ${WM_TOKEN}`,
-      'Content-Type': 'application/json'
+      Authorization: `Bearer ${WM_TOKEN}`,
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   });
 
   return {
-    "leadmagic_api_key": correct_api_key
-  }
+    leadmagic_api_key: correct_api_key,
+  };
 }
-
