4e8d28242899712acc4087c9b378b728941a79d9
admin <admin@windmill.dev>
2024-05-25 19:15:11 +0000

    Flow 'f/bluewind/master_flow' deployed

--- windmill/f/bluewind/master_flow.flow/create_campaign_on_twenty.inline_script.native.ts
+++ windmill/f/bluewind/master_flow.flow/create_campaign_on_twenty.inline_script.native.ts
@@ -1,4 +1,5 @@
 export async function main(twenty_api_key: string, campaign_name: string) {
+  name%5Beq%5D%3A%22${encodedCampaignName}
   const url = 'https://api.twenty.com/rest/campaigns';
   const options = {
     method: 'POST',
@@ -8,10 +9,10 @@ export async function main(twenty_api_key: string, campaign_name: string) {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      campaign_name: campaign_name
+      name: campaign_name
     })
   };
 
   const response = await fetch(url, options);
-  return await response.json();
+  return await response.json()
 }
