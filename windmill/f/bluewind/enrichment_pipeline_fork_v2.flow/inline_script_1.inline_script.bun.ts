ddf992e30889859c5f982e8db5709be0b3ec1bac
merwanehamadi <merwanehamadi@gmail.com>
2024-05-25 13:26:30 +0000

    Flow 'f/bluewind/enrichment_pipeline_fork_v2' deployed

--- windmill/f/bluewind/enrichment_pipeline_fork_v2.flow/inline_script_1.inline_script.bun.ts
+++ windmill/f/bluewind/enrichment_pipeline_fork_v2.flow/inline_script_1.inline_script.bun.ts
@@ -1,6 +1,8 @@
 // Fetch-only script, no imports allowed but benefits from a dedicated highly efficient runtime
 
 export async function main() {
-  // "3" is the default value of example_input, it can be overriden with code or using the UI
-  return {}
+  return {
+    "isEmailCatchAll": true,
+    "isEmailValid": true,
+  }
 }
