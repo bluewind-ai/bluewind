06be616e50bec53a01dd461cc0668b18a6f0fccb
merwanehamadi <merwanehamadi@gmail.com>
2024-05-25 15:26:39 +0000

    Flow 'f/bluewind/enrichment_pipeline_fork' deleted

--- windmill/f/bluewind/enrichment_pipeline_fork.flow/enrich_is_catch_all.inline_script.native.ts
+++
@@ -1,8 +0,0 @@
-// Fetch-only script, no imports allowed but benefits from a dedicated highly efficient runtime
-
-export async function main() {
-  return {
-    property: "is_catch_all",
-    value: "true",
-  }
-}
