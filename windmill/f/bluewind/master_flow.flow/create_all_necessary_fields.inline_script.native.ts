fb09afcf39a3d29ec598ce23c23bf2a7d92154ff
merwanehamadi <merwanehamadi@gmail.com>
2024-05-25 21:45:55 +0000

    Flow 'f/bluewind/master_flow' deployed

--- windmill/f/bluewind/master_flow.flow/create_all_necessary_fields.inline_script.native.ts
+++ windmill/f/bluewind/master_flow.flow/create_all_necessary_fields.inline_script.native.ts
@@ -115,5 +115,55 @@ export async function main(twenty_api_key: string) {
     })
   });
 
+  variables = {
+    input: {
+      field: {
+        "defaultValue": "'SOURCED'",
+        "description": "Status of this contact in the curent campaign",
+        "icon": "IconUsers",
+        "label": "Campaign Status",
+        "name": "campaignStatus",
+        "options": [
+          {
+            "color": "blue",
+            "id": "5817df78-228d-4d17-a7eb-7875d380034d",
+            "label": "Sourced",
+            "position": 0,
+            "value": "SOURCED"
+          },
+          {
+            "color": "turquoise",
+            "id": "cd2d4f3f-1404-483a-acf7-935b06fea8d8",
+            "label": "Unqualified",
+            "position": 1,
+            "value": "UNQUALIFIED"
+          },
+          {
+            "color": "green",
+            "id": "ccf00459-c395-4c7f-aaa0-44cf960805e9",
+            "label": "Email Scheduled",
+            "position": 2,
+            "value": "EMAIL_SCHEDULED"
+          }
+        ],
+        "objectMetadataId": "e92cc3f7-959f-4389-9348-b41a9a8b07ef",
+        "type": "SELECT"
+      }
+    }
+  };
+
+
+  response = await fetch('https://api.twenty.com/metadata', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${twenty_api_key}`,
+    },
+    body: JSON.stringify({
+      query: mutation,
+      variables: variables
+    })
+  });
+
   return await response.json();
 }
