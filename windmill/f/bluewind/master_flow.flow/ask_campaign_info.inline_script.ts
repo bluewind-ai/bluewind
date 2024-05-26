b1f7cc17e696362b30a234e190a65a4c44c8cb54
merwanehamadi <merwanehamadi@gmail.com>
2024-05-26 17:35:39 +0000

    Flow 'f/bluewind/master_flow' deployed

--- windmill/f/bluewind/master_flow.flow/ask_campaign_info.inline_script.ts
+++ windmill/f/bluewind/master_flow.flow/ask_campaign_info.inline_script.ts
@@ -1,17 +1,16 @@
 import * as wmill from "npm:windmill-client@^1.158.2";
 
 export async function main() {
-  // if no argument is passed, if user is logged in, it will use the user's username
   const resumeUrls = await wmill.getResumeUrls("approver1");
-
-  // send the resumeUrls to the recipient or see Prompt section above
 
   return {
     resume: resumeUrls["resume"],
     default_args: {
       instructions:
-        "Please copy paste the JSON in an code editor and modify it for your campaign",
+        "please copy this json in an editor and modify it to suit your needs",
       campaign_info: {
+        "recipient_first_name": "Steven",
+        "recipient_company_name": "First Round",
         "sender_company_name": "Bluewind",
         "custom_subject_lines": [
           "Bluewind pre-seed",
@@ -26,11 +25,10 @@ export async function main() {
         "block_1": "Bluewind is raising a pre-seed. 100k at 5M.",
         "block_2": `About Bluewind:
 - open-source go-to-market assistant to generate leads.
-- my customers were overwhelmed by all the GTM tools and wasted time and money trying all of them.
-- Instead of creating another tool we created an assistant that connects all the best SaaS together to generate leads for them.
-- about me: ex AutoGPT, AI Agent expert(author of paper published at Neurips), and famous Growth Hacker.`,
-        "block_3":
-          "Would you like me to send you the deck? I don't send it directly because of deliverability.",
+- our customers were overwhelmed by all the GTM tools and wasted time and money trying all of them.
+- Instead of creating another tool we created an assistant that connects all the best SaaS together to generate leads for them
+- about me: ex AutoGPT, AI Agent expert (author of paper published at Neurips), and famous Growth Hacker.",
+  "block_3": "Would you like me to send you the deck? I don't send it directly because of deliverability.`,
         "block_4": "",
         "block_5": "",
       },
--- windmill/f/bluewind/master_flow.flow/get_campaign_name_2.inline_script.native.ts
+++ windmill/f/bluewind/master_flow.flow/get_campaign_name_2.inline_script.native.ts
@@ -1,5 +1,3 @@
-// Fetch-only script, no imports allowed but benefits from a dedicated highly efficient runtime
-
 export async function main(campaign_name: string) {
   return {
     campaign_name
