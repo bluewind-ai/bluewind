14ba31c8c169e7dedbdafac5bc28813f2badd03d
merwanehamadi <merwanehamadi@gmail.com>
2024-05-26 16:36:05 +0000

    Script 'f/bluewind/master_flow/ch' deployed

---
+++ windmill/f/bluewind/master_flow/ch.fetch.ts
@@ -0,0 +1,17 @@
+export async function main(twenty_api_key: string, campaign_name: string) {
+  if (!campaign_name) {
+    throw new Error("Campaign name is required.");
+  }
+
+  const url = `https://api.twenty.com/rest/people?filter=campaignName[eq]:"${encodeURIComponent(campaign_name)}"&campaignStatus[eq]:"SOURCED"`;
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${twenty_api_key}`
+    }
+  });
+
+  return await response.json();
+}
