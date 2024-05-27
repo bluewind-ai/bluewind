e880ef61b99b2f06dd48a5c2d548f42bfbb86061
merwanehamadi <merwanehamadi@gmail.com>
2024-05-27 15:08:35 +0000

    Flow 'f/bluewind/subject_lines_without_company' deleted

--- windmill/f/bluewind/subject_lines_without_company.flow/generate_subject_lines_without_company_name.inline_script.native.ts
+++
@@ -1,8 +0,0 @@
-export async function main(recipient_first_name: string, recipient_company_name: string): Promise<any> {
-  return {
-    "subject_lines": [
-      "quick question",
-      `${recipient_first_name}, thoughts?`,
-    ]
-  }
-}
