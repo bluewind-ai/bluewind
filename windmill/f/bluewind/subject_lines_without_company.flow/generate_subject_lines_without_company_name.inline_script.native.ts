82e1b3582dc22277046fc09703a2b71784fcd966
merwanehamadi <merwanehamadi@gmail.com>
2024-05-27 15:07:48 +0000

    Flow 'f/bluewind/subject_lines_without_company' deployed

--- windmill/f/bluewind/subject_lines_without_company.flow/generate_subject_lines_without_company_name.inline_script.native.ts
+++ windmill/f/bluewind/subject_lines_without_company.flow/generate_subject_lines_without_company_name.inline_script.native.ts
@@ -1,6 +1,3 @@
-// Since there are no specific resource types mentioned, we will not define any resource types here.
-// This function simply returns the inputs it receives.
-
 export async function main(recipient_first_name: string, recipient_company_name: string): Promise<any> {
   return {
     "subject_lines": [
