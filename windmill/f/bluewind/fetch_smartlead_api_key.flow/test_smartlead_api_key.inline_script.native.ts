138e9d755b38a7f50ef28a6c3d1f4010ea05afdd
merwanehamadi <merwanehamadi@gmail.com>
2024-05-27 17:01:52 +0000

    Flow 'f/bluewind/fetch_smartlead_api_key' deleted

--- windmill/f/bluewind/fetch_smartlead_api_key.flow/test_smartlead_api_key.inline_script.native.ts
+++ windmill/f/bluewind/fetch_smartlead_api_key.flow/test_smartlead_api_key.inline_script.native.ts
@@ -1,5 +1,5 @@
 export async function main(smartlead_api_key: string) {
-  const url = 'https://api.twenty.com/rest/apiKeys';
+  const url = 'https://server.smartlead.ai/api/v1/email-accounts?api_key=ea5efb9a-f162-464f-aa0b-88e8820d81ac_zrtdya5&limit=1';
   const options = {
     method: 'GET',
     headers: {
