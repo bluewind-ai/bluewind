6f7d1dbf38ab059cc65809de8ead1594863f5eaa
merwanehamadi <merwanehamadi@gmail.com>
2024-05-27 17:02:14 +0000

    Flow 'f/bluewind/fetch_smartlead_api_key' deployed

--- windmill/f/bluewind/fetch_smartlead_api_key.flow/test_smartlead_api_key.inline_script.native.ts
+++ windmill/f/bluewind/fetch_smartlead_api_key.flow/test_smartlead_api_key.inline_script.native.ts
@@ -1,10 +1,9 @@
 export async function main(smartlead_api_key: string) {
-  const url = 'https://server.smartlead.ai/api/v1/email-accounts?api_key=ea5efb9a-f162-464f-aa0b-88e8820d81ac_zrtdya5&limit=1';
+  const url = `https://server.smartlead.ai/api/v1/email-accounts?api_key=${smartlead_api_key}&limit=1`;
   const options = {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
-      'Authorization': `Bearer ${smartlead_api_key}`,
       'Content-Type': 'application/json'
     }
   };
