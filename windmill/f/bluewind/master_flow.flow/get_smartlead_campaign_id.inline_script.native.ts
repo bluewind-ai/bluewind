ae96378962376c9cbc4eb551bd14fe7167f6a687
merwanehamadi <merwanehamadi@gmail.com>
2024-05-28 12:56:03 +0000

    Flow 'f/bluewind/master_flow' deployed

--- windmill/f/bluewind/master_flow.flow/get_smartlead_campaign_id.inline_script.native.ts
+++ windmill/f/bluewind/master_flow.flow/get_smartlead_campaign_id.inline_script.native.ts
@@ -1,7 +1,7 @@
 // Fetch-only script, no imports allowed but benefits from a dedicated highly efficient runtime
 
-export async function main(smartlead: object) {
+export async function main(smartlead_campaign_id: object) {
   return {
-    smartlead_campaign_id: smartlead_response.campaign_id
+    smartlead_campaign_id
   }
 }
