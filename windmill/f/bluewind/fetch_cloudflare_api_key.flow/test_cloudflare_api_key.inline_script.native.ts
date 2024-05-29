07f3f96dbab9135f64fb6f22a0c0de97a44c11e0
merwanehamadi <merwanehamadi@gmail.com>
2024-05-29 11:47:52 +0000

    Flow 'f/bluewind/fetch_cloudflare_api_key' deployed

--- windmill/f/bluewind/fetch_cloudflare_api_key.flow/test_cloudflare_api_key.inline_script.native.ts
+++ windmill/f/bluewind/fetch_cloudflare_api_key.flow/test_cloudflare_api_key.inline_script.native.ts
@@ -1,17 +1,18 @@
-export async function main(smartlead_api_key: string) {
+export async function main(cloudflare_api_key: string) {
   const url = `https://api.cloudflare.com/client/v4/user`;
   const options = {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'X-Auth-Key': cloudflare_api_key
     }
   };
   try {
     const response = await fetch(url, options);
     if (response.status === 200) {
       return {
-        smartlead_api_key
+        cloudflare_api_key
       }
     } else {
       return {}
