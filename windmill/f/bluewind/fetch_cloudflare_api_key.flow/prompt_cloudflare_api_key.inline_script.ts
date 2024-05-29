7532cb23a6190823611ce01d532f0fbaef430034
merwanehamadi <merwanehamadi@gmail.com>
2024-05-29 15:52:44 +0000

    Flow 'f/bluewind/fetch_cloudflare_api_key' deployed

--- windmill/f/bluewind/fetch_cloudflare_api_key.flow/prompt_cloudflare_api_key.inline_script.ts
+++ windmill/f/bluewind/fetch_cloudflare_api_key.flow/prompt_cloudflare_api_key.inline_script.ts
@@ -6,6 +6,8 @@ export async function main() {
   return {
     resume: resumeUrls["resume"],
     default_args: {
+      instructions:
+        "Create the cloudflare api token: https://www.loom.com/share/b6210ab42c194118985455ce01abb2f",
       cloudflare_api_key: "YOUR_CLOUDFLARE_API_KEY",
     },
   };
