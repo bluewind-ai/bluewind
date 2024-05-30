7f2410b2bb2c6261261a5e66023216ba51c7d7e7
merwanehamadi <merwanehamadi@gmail.com>
2024-05-30 19:28:56 +0000

    Flow 'f/bluewind/setup_email_accounts' deployed

--- windmill/f/bluewind/setup_email_accounts.flow/get_domain_name_info.inline_script.native.ts
+++
@@ -1,7 +0,0 @@
-export async function main(all_attempts: Array<string>) {
-  if (all_attempts.length === 0) {
-    return "No attempts found.";
-  }
-
-  return all_attempts[all_attempts.length - 1];
-}
--- windmill/f/bluewind/setup_email_accounts.flow/get_primary_domain_name.inline_script.native.ts
+++
@@ -1,7 +0,0 @@
-// Fetch-only script, no imports allowed but benefits from a dedicated highly efficient runtime
-
-export async function main(primary_domain_name: string) {
-  return {
-    primary_domain_name
-  }
-}
