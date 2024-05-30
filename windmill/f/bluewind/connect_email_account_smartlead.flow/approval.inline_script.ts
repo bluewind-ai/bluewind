7842a7e04c6c3ed1d3343d68dc61a61da1e785fe
merwanehamadi <merwanehamadi@gmail.com>
2024-05-30 13:35:41 +0000

    Flow 'f/bluewind/connect_email_account_smartlead' deployed

--- windmill/f/bluewind/connect_email_account_smartlead.flow/approval.inline_script.ts
+++ windmill/f/bluewind/connect_email_account_smartlead.flow/approval.inline_script.ts
@@ -1,6 +1,6 @@
 import * as wmill from "npm:windmill-client@^1.158.2";
 
-export async function main(domain_just_created: string) {
+export async function main() {
   // if no argument is passed, if user is logged in, it will use the user's username
   const resumeUrls = await wmill.getResumeUrls("approver1");
 
