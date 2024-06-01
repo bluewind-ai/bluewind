1f5f2950aee54390ba3b9cb0f12b50ed8f6a3910
merwanehamadi <merwanehamadi@gmail.com>
2024-06-01 15:13:12 +0000

    Flow 'f/bluewind/get_contacts_to_outreach' deployed

---
+++ windmill/f/bluewind/get_contacts_to_outreach.flow/get_contacts_to_outreach.inline_script.native.ts
@@ -0,0 +1,65 @@
+export async function main(twenty_api_key: string, campaign_name: string) {
+  const query = `query FindManyPeople($filter: PersonFilterInput) {
+  people(filter: $filter) {
+    totalCount
+    __typename
+    edges {
+      node {
+        	name {
+          firstName
+          lastName
+        }
+        email
+          linkedinLink{
+          label
+          url
+        }
+        jobTitle
+        phone
+        city
+        avatarUrl
+        position
+        id
+        createdAt
+        updatedAt
+        companyId
+        isEmailCatchAll
+        campaignStatus
+        isEmailValid
+        campaignName
+      }
+    }
+  }
+}
+`;
+
+  let variables = {
+    "filter": {
+      "campaignStatus": {
+        "in": [
+          "SOURCED"
+        ]
+      },
+      "campaignName": {
+        "eq":
+          campaign_name
+      }
+    }
+  };
+
+  let response = await fetch('https://api.twenty.com/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${twenty_api_key}`,
+
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+
+  return await response.json()
+}
