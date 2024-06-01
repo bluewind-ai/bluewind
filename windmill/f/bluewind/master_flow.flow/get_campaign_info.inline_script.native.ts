e0de40ad8d94ebcb06cb4a7b1830b112606c5117
merwanehamadi <merwanehamadi@gmail.com>
2024-06-01 14:21:37 +0000

    Flow 'f/bluewind/master_flow' deployed

--- windmill/f/bluewind/master_flow.flow/get_campaign_info.inline_script.native.ts
+++
@@ -1,7 +0,0 @@
-// Fetch-only script, no imports allowed but benefits from a dedicated highly efficient runtime
-
-export async function main(campaign_info: Object) {
-  return {
-    campaign_info
-  }
-}
