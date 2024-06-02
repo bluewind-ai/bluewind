5cd9cf9a3ad6bda7255b078f66fa7e17e93f337c
merwanehamadi <merwanehamadi@gmail.com>
2024-06-02 19:52:30 +0000

    Flow 'f/bluewind/migrate_twenty_fields_if_needed' deployed

--- windmill/f/bluewind/migrate_twenty_fields_if_needed.flow/get_expected_last_twenty_migration_date.inline_script.native.ts
+++ windmill/f/bluewind/migrate_twenty_fields_if_needed.flow/get_expected_last_twenty_migration_date.inline_script.native.ts
@@ -3,6 +3,6 @@
 
 export async function main(example_input: number = 3) {
   return {
-    expected_last_twenty_migration_date: "2024-06-02T12:34:56Z"
+    expected_last_twenty_migration_date: "2024-06-02T19:52:23Z"
   }
 }
