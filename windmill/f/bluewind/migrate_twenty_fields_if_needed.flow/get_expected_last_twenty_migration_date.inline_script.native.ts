7544f7bc064c136c60cd4060ea734b70fa2657d6
merwanehamadi <merwanehamadi@gmail.com>
2024-06-02 19:51:58 +0000

    Flow 'f/bluewind/migrate_twenty_fields_if_needed' deleted

---
+++ windmill/f/bluewind/migrate_twenty_fields_if_needed.flow/get_expected_last_twenty_migration_date.inline_script.native.ts
@@ -0,0 +1,9 @@
+// Fetch-only script, no imports allowed (except windmill) but benefits from a dedicated highly efficient runtime
+//import * as wmill from './windmill.ts'
+
+export async function main(example_input: number = 3) {
+  return {
+    expected_last_twenty_migration_date: "2024-06-02T12:34:56Z"
+
+  }
+}
