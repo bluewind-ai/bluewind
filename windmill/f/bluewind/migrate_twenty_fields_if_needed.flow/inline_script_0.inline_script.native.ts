de1d8de714d2d757e67c30dccb171c6a8ce67326
merwanehamadi <merwanehamadi@gmail.com>
2024-06-02 21:01:55 +0000

    Flow 'f/bluewind/migrate_twenty_fields_if_needed' deployed

--- windmill/f/bluewind/migrate_twenty_fields_if_needed.flow/inline_script_0.inline_script.native.ts
+++ windmill/f/bluewind/migrate_twenty_fields_if_needed.flow/inline_script_0.inline_script.native.ts
@@ -3,6 +3,11 @@ export async function main() {
     const response = await fetch(`${BASE_INTERNAL_URL}/api/w/${WM_WORKSPACE}/flows/get/f/bluewind/migrate_twenty_fields_if_needed`, {
       headers: { Authorization: `Bearer ${WM_TOKEN}` }
     });
+    if (!response.ok) {
+      const data = await response.json();
+
+      throw new Error(`HTTP error! status: ${response.status} error: ${JSON.stringify(data.errors, null, 2)}`);
+    }
   } catch (error) {
     throw error;
   }
