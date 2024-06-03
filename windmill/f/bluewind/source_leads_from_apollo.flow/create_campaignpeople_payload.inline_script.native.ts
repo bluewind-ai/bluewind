7defa9606cdbb2271a7bfe00ebc29dfe35662911
merwanehamadi <merwanehamadi@gmail.com>
2024-06-03 22:43:40 +0000

    Flow 'f/bluewind/source_leads_from_apollo' deployed

--- windmill/f/bluewind/source_leads_from_apollo.flow/create_campaignpeople_payload.inline_script.native.ts
+++ windmill/f/bluewind/source_leads_from_apollo.flow/create_campaignpeople_payload.inline_script.native.ts
@@ -1,8 +1,9 @@
 // Fetch-only script, no imports allowed but benefits from a dedicated highly efficient runtime
-export async function main(data: Array, campaign_name: string) {
+export async function main(data: Array, campaign_name: string, twenty_campaign_id: string) {
   return data.data.createPeople.map(item => ({
+    name: `${campaign_name} ${item.name.firstName} ${item.name.lastName}`,
     personId: item.id,
-    campaignId: "873a1e7e-987a-4f4f-8344-c3d7a8bc8a5d",
+    campaignId: twenty_campaign_id,
     campaignStatus: "SOURCED"
   }))
 }
