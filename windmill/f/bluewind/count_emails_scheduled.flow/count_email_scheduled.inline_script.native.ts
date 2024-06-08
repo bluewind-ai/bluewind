4fa3d08b19cd151425e56d0198d9fda83f555f10
merwanehamadi <merwanehamadi@gmail.com>
2024-06-08 15:55:36 +0000

    Flow 'f/bluewind/count_emails_scheduled' deployed

--- windmill/f/bluewind/count_emails_scheduled.flow/count_email_scheduled.inline_script.native.ts
+++ windmill/f/bluewind/count_emails_scheduled.flow/count_email_scheduled.inline_script.native.ts
@@ -1,25 +1,18 @@
 export async function main(nocodb: Object) {
+  const url = `${nocodb.apiUrl}/api/v2/tables/m59o2tuojdl19od/records?where=${encodeURIComponent("(status,eq,SCHEDULED)")}&fields=Id`;
 
-  const url = `${nocodb.apiUrl}/api/v2/tables/{tableId}/records`;
   const options = {
     method: 'GET',
     headers: {
-      "xc-auth": `Bearer ${nocodb.xc_token}`,
+      "xc-token": `${nocodb.xc_token}`,
       'Content-Type': 'application/json',
+      'Accept': "application/json"
     },
   };
   try {
     const response = await fetch(url, options);
     return await response.json()
-    if (!response.ok) {
-      return "ok"
-      return {};
-    }
-    return {
-      xc_token: nocodb.xc_token,
-      apiUrl: nocodb.apiUrl
-    };
   } catch (error) {
-    return {};
+    return error;
   }
 }
