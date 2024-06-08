cdccde28250619d30ee405a4b905006605b77177
merwanehamadi <merwanehamadi@gmail.com>
2024-06-08 16:39:40 +0000

    Flow 'f/bluewind/schedule_more_emails' deployed

---
+++ windmill/f/bluewind/schedule_more_emails.flow/inline_script_0.inline_script.native.ts
@@ -0,0 +1,18 @@
+export async function main(nocodb: Object) {
+  const url = `${nocodb.apiUrl}/api/v2/tables/mmbbdm5ek99c71g/records?fields=Id,name`;
+
+  const options = {
+    method: 'GET',
+    headers: {
+      "xc-token": `${nocodb.xc_token}`,
+      'Content-Type': 'application/json',
+      'Accept': "application/json"
+    },
+  };
+  try {
+    const response = await fetch(url, options);
+    return await response.json()
+  } catch (error) {
+    return error;
+  }
+}
