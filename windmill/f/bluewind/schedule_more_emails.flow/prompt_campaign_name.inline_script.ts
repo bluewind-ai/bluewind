5056722afaab07709a51d86f0f0a28579d01f49f
merwanehamadi <merwanehamadi@gmail.com>
2024-06-08 17:27:44 +0000

    Flow 'f/bluewind/schedule_more_emails' deployed

--- windmill/f/bluewind/schedule_more_emails.flow/prompt_campaign_name.inline_script.ts
+++ windmill/f/bluewind/schedule_more_emails.flow/prompt_campaign_name.inline_script.ts
@@ -2,7 +2,7 @@
 
 import * as wmill from "npm:windmill-client@^1.158.2";
 
-export async function main(campaigns: Array) {
+export async function main() {
   // if no argument is passed, if user is logged in, it will use the user's username
   const resumeUrls = await wmill.getResumeUrls("approver1");
 
