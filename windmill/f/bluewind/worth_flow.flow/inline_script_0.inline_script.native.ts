c1c30ebb46f59eb8e545b8038a3b86ae68d752c1
merwanehamadi <merwanehamadi@gmail.com>
2024-06-09 00:23:37 +0000

    Flow 'f/bluewind/worth_flow' deployed

--- windmill/f/bluewind/worth_flow.flow/inline_script_0.inline_script.native.ts
+++ windmill/f/bluewind/worth_flow.flow/inline_script_0.inline_script.native.ts
@@ -2,9 +2,7 @@
 //import * as wmill from './windmill.ts'
 
 export async function main(example_input: nocodb = 3) {
-  // "3" is the default value of example_input, it can be overriden with code or using the UI
-  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${example_input}`, {
-    headers: { "Content-Type": "application/json" },
-  });
-  return res.json();
+  {
+    "ok": "ok"
+  }
 }
