b6006da67494f4e79ddf43c76ab52a0949f4cf23
merwanehamadi <merwanehamadi@gmail.com>
2024-06-09 00:34:49 +0000

    Flow 'f/bluewind/worth_flow' deployed

---
+++ windmill/f/bluewind/worth_flow.flow/approval.inline_script.ts
@@ -0,0 +1,17 @@
+//this assumes the Form tab has a string field named "foo" and a checkbox named "bar"
+
+import * as wmill from "npm:windmill-client@^1.158.2";
+
+export async function main() {
+  // if no argument is passed, if user is logged in, it will use the user's username
+  const resumeUrls = await wmill.getResumeUrls("approver1");
+
+  // send the resumeUrls to the recipient or see Prompt section above
+
+  return {
+    resume: resumeUrls["resume"],
+    default_args: {
+      test: "YOUR_TWENTY_API_KEY",
+    }, // optional
+  };
+}
