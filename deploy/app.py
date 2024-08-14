464cb1ccf00d9e2c4504658eb9210725c5a0ea70
Merwane Hamadi <merwanehamadi@gmail.com>
2024-08-14 16:01:31 -0700

    make the account id dynamic

--- deploy/app.py
+++ deploy/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 from aws_cdk import App, Environment
 from django_fargate_cdk_stack import SimpleFargateCdkStack
 from dotenv import load_dotenv
@@ -10,7 +11,7 @@ app = App()
 
 SimpleFargateCdkStack(app, "DjangoFargateCdkStack6",
     env=Environment(
-        account="361769569102",
+        account=os.getenv("AWS_ACCOUNT_ID"),
         region="us-west-2"
     ),
 )
