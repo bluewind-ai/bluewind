d83735f10aa56d7cdd20f4a348c151c6e5675ce1
Merwane Hamadi <merwanehamadi@gmail.com>
2024-08-14 19:04:21 -0700

    Revert "remove nat gateways"
    This reverts commit 2ceafa6454cef0d41387f8d58049886f4071f838.

--- deploy/django_fargate_cdk_stack.py
+++ deploy/django_fargate_cdk_stack.py
@@ -67,17 +67,7 @@ class SimpleFargateCdkStack(Stack):
             certificate_arn=config["certificate_arn"]
         )
 
-        vpc = ec2.Vpc(self, "MyVPC", 
-            max_azs=config["max_azs"],
-            nat_gateways=0,
-            subnet_configuration=[
-                ec2.SubnetConfiguration(
-                    name="public",
-                    subnet_type=ec2.SubnetType.PUBLIC,
-                    cidr_mask=24
-                )
-            ]
-        )
+        vpc = ec2.Vpc(self, "MyVPC", max_azs=config["max_azs"])
 
         cluster = ecs.Cluster(self, "MyCluster", vpc=vpc)
         
@@ -160,8 +150,6 @@ class SimpleFargateCdkStack(Stack):
                 },
             ),
             public_load_balancer=True,
-            assign_public_ip=True,  # Add this line
-            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
             health_check_grace_period=Duration.seconds(60),  # This is now in the correct place
         )
 
@@ -233,3 +221,6 @@ class SimpleFargateCdkStack(Stack):
         fargate_service.task_definition.default_container.add_environment(
             "STATIC_URL", f"https://{config['domain_name']}/staticfiles/,https://{distribution.distribution_domain_name}/staticfiles/"
         )
+
+        # Grant the Fargate task permission to access the RDS instance
+        db_instance.connections.allow_default_port_from(fargate_service.service.connections)
