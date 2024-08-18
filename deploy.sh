11f3fe8927f94bb683a1a0da74ad67cc3626b4ec
Merwane Hamadi <merwanehamadi@gmail.com>
2024-08-18 11:18:42 -0700

    postpone the dev environment

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,19 @@ run_docker_tests() {
     echo "Docker tests completed."
 }
 
+# Function to run OpenTofu commands
+run_opentofu() {
+    cd opentf_deploy
+    echo "Logging in to AWS SSO and setting up environment variables..."
+    aws sso login --profile ci-cd-admin && 
+    eval "$(aws configure export-credentials --profile ci-cd-admin --format env)" && 
+    export TF_VAR_aws_access_key_id=$AWS_ACCESS_KEY_ID TF_VAR_aws_secret_access_key=$AWS_SECRET_ACCESS_KEY TF_VAR_aws_session_token=$AWS_SESSION_TOKEN && 
+    echo "AWS and Terraform environment variables have been set."
+
+    tofu apply --auto-approve
+    echo "OpenTofu apply completed."
+}
+
 # Run local tests in background
 run_local_tests &
 local_pid=$!
@@ -30,19 +43,13 @@ local_pid=$!
 run_docker_tests &
 docker_pid=$!
 
-# Wait for both processes to complete
+# Run OpenTofu commands in background
+run_opentofu &
+opentofu_pid=$!
+
+# Wait for all processes to complete
 wait $local_pid
 wait $docker_pid
+wait $opentofu_pid
 
-
-
-cd opentf_deploy
-# AWS SSO login and environment setup
-echo "Logging in to AWS SSO and setting up environment variables..."
-aws sso login --profile ci-cd-admin && 
-eval "$(aws configure export-credentials --profile ci-cd-admin --format env)" && 
-export TF_VAR_aws_access_key_id=$AWS_ACCESS_KEY_ID TF_VAR_aws_secret_access_key=$AWS_SECRET_ACCESS_KEY TF_VAR_aws_session_token=$AWS_SESSION_TOKEN && 
-echo "AWS and Terraform environment variables have been set."
-
-tofu apply
 echo "All steps completed."
