fce708357b6a74b565dfa06cbf435b36a220ade7
Merwane Hamadi <merwanehamadi@gmail.com>
2024-08-26 20:42:58 -0700

    rewrite pg_sync to recreate db

--- pg_sync.sh
+++ pg_sync.sh
@@ -2,12 +2,12 @@
 
 # Run pg_dump on the replica
 
-PGPASSWORD=$REPLICA_PASSWORD /opt/homebrew/opt/postgresql@16/bin/pg_dump -h $REPLICA_HOST -U $REPLICA_USER -d $REPLICA_DB >backup.sql
+# PGPASSWORD=$REPLICA_PASSWORD /opt/homebrew/opt/postgresql@16/bin/pg_dump -h $REPLICA_HOST -U $REPLICA_USER -d $REPLICA_DB >backup.sql
 
 # Load the dump into the target database
-PGPASSWORD=$TARGET_PASSWORD dropdb -h $TARGET_HOST -U $TARGET_USER $TARGET_DB
-PGPASSWORD=$TARGET_PASSWORD createdb -h $TARGET_HOST -U $TARGET_USER $TARGET_DB
-PGPASSWORD=$TARGET_PASSWORD psql -h $TARGET_HOST -U $TARGET_USER -d $TARGET_DB <backup.sql
+PGPASSWORD=$DB_PASSWORD dropdb -h $DB_HOST -U $DB_USERNAME $DB_NAME
+PGPASSWORD=$DB_PASSWORD createdb -h $DB_HOST -U $DB_USERNAME $DB_NAME
+# PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USERNAME -d $DB_NAME <backup.sql
 
 # Clean up
 # rm backup.sql
