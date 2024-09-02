228db3f9272c6d19398d9e73ac504ad419ee0199
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-02 12:34:21 -0700

    remove the need for a prefix in the mode

--- base_model/apps.py
+++ base_model/apps.py
@@ -1,16 +1,6 @@
 from django.apps import AppConfig
-from public_id.models import PREFIX_MAPPINGS
 
 
 class BaseModelConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "base_model"
-
-    def ready(self):
-        from django.apps import apps
-
-        for model in apps.get_models():
-            if model.__name__ not in PREFIX_MAPPINGS:
-                raise ValueError(
-                    f"You didn't give a prefix for the model: {model.__name__}"
-                )
