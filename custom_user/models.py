897efa3494f531e4e70a494c49434ab7b3cabe53
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-01 17:27:24 -0700

    make users inherit the BaseModel

--- custom_user/models.py
+++ custom_user/models.py
@@ -1,9 +1,6 @@
+from base_model.models import BaseModel
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 
-class User(AbstractUser):
-    workspace_public_id = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.username
+class User(AbstractUser, BaseModel):
+    pass
