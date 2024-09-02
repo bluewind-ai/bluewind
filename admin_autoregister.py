c7c3fbc8ceb698afddbc1d8729a89dc9aedd94bb
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-01 21:36:25 -0700

    autoregistration should still not ignore admin customization

--- admin_autoregister.py
+++ admin_autoregister.py
@@ -1,23 +1,51 @@
+import sys
+
 from bluewind.admin_site import custom_admin_site
 from django.apps import apps
 from django.contrib import admin
 from django.contrib.admin.sites import AlreadyRegistered
-
-# Explicitly import the models
 from django.db.migrations.recorder import MigrationRecorder
 
 
 def autoregister():
-    # Your existing auto-registration code
     for app_config in apps.get_app_configs():
+        app_label = app_config.label
         for model in app_config.get_models():
+            admin_class = None
+            model_name = model.__name__
+            admin_class_name = f"{model_name}Admin"
+
+            # Try to find a custom Admin class in the same module as the model
+            model_module = sys.modules[model.__module__]
+            if hasattr(model_module, admin_class_name):
+                admin_class = getattr(model_module, admin_class_name)
+
+            # If no custom Admin class found, check in a separate admin.py file
+            if not admin_class:
+                try:
+                    admin_module = __import__(
+                        f"{app_label}.admin", fromlist=[admin_class_name]
+                    )
+                    if hasattr(admin_module, admin_class_name):
+                        admin_class = getattr(admin_module, admin_class_name)
+                except ImportError:
+                    pass
+
+            # Register with custom admin site
             try:
-                custom_admin_site.register(model)
+                if admin_class:
+                    custom_admin_site.register(model, admin_class)
+                else:
+                    custom_admin_site.register(model)
             except AlreadyRegistered:
                 pass
 
+            # Register with default admin site
             try:
-                admin.site.register(model)
+                if admin_class:
+                    admin.site.register(model, admin_class)
+                else:
+                    admin.site.register(model)
             except AlreadyRegistered:
                 pass
 
