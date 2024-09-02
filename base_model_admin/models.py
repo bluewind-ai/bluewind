32a4efafe08f9f246a6eadeb71f4586b027aba35
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-02 12:34:53 -0700

    BaseAdmin incorrectly setup

--- base_model_admin/models.py
+++ base_model_admin/models.py
@@ -2,14 +2,6 @@ from django.contrib import admin
 
 
 class BaseAdmin(admin.ModelAdmin):
-    def get_readonly_fields(self, request, obj=None):
-        # By default, make all fields readonly
-        return [field.name for field in self.model._meta.fields]
-
     def get_list_display(self, request):
         # By default, display all fields in the list view
-        return [field.name for field in self.model._meta.fields]
-
-    def get_fields(self, request, obj=None):
-        # By default, include all fields in the detail view
         return [field.name for field in self.model._meta.fields]
