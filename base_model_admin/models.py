616ab678dfcd3303733d05754f1a65a847d4cb30
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-01 21:36:54 -0700

    workspace should be preselected + remove save_model

--- base_model_admin/models.py
+++ base_model_admin/models.py
@@ -6,10 +6,8 @@ class BaseAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         workspace_public_id = request.environ["WORKSPACE_PUBLIC_ID"]
-        # return qs
         if self.model == Workspace:
             return qs.filter(public_id=workspace_public_id)
-
         return qs.filter(workspace__public_id=workspace_public_id)
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
@@ -17,17 +15,10 @@ class BaseAdmin(admin.ModelAdmin):
 
         if db_field.name == "workspace":
             kwargs["queryset"] = Workspace.objects.filter(public_id=workspace_public_id)
+            kwargs["initial"] = Workspace.objects.get(public_id=workspace_public_id)
         elif hasattr(db_field.related_model, "workspace"):
             kwargs["queryset"] = db_field.related_model.objects.filter(
                 workspace__public_id=workspace_public_id
             )
 
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
-    def save_model(self, request, obj, form, change):
-        if not hasattr(obj, "workspace") or not obj.workspace:
-            workspace = Workspace.objects.get(
-                public_id=request.environ["WORKSPACE_PUBLIC_ID"]
-            )
-            obj.workspace = workspace
-        super().save_model(request, obj, form, change)
