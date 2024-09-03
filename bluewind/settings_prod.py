bc283299333ad0f8918244e1c14c2947e88da16b
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-03 16:47:20 -0700

    checkpoint

--- bluewind/settings_prod.py
+++ bluewind/settings_prod.py
@@ -1,73 +1,80 @@
-# from local_secrets import load_secrets_to_env
-
-
-# load_secrets_to_env("prod-env")
 import os
 from pathlib import Path
 
 import environ
 
-# env = environ.Env(
-#     # set casting, default value
-#     DEBUG=(bool, False)
-# )
-
+# Base Directory
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# Core Django Settings
+SECRET_KEY = os.environ["SECRET_KEY"]
+DEBUG = os.environ["DEBUG"]
+ALLOWED_HOSTS = environ.Env().list("ALLOWED_HOSTS")
+ROOT_URLCONF = "bluewind.urls"
+WSGI_APPLICATION = "bluewind.wsgi.application"
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
+# Application Definition
 INSTALLED_APPS = [
-    # 'django_extensions',
-    "django.contrib.sessions",
-    "django.db.migrations",
-    "users",
-    "allauth_ui",
-    "model_clone",
+    # Django core apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
-    ## User sessions
-    "api_providers",
-    ## User sessions
+    "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "workspaces",
-    "base_model_admin",
-    "auto_tests",
-    # debugging
     "django.contrib.sites",
+    "django.db.migrations",
+    # Third-party apps
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
-    # LOCAL APPS'
+    "allauth.socialaccount.providers.google",
+    "allauth_ui",
+    "behave_django",
+    "channels",
+    "debug_toolbar",
+    "django_extensions",
+    "django_object_actions",
+    "model_clone",
+    "slippers",
+    "widget_tweaks",
+    # Local apps
+    "users",
+    "api_providers",
+    "workspaces",
+    "base_model_admin",
+    "auto_tests",
     "chat_messages",
     "people",
-    # tests
-    "behave_django",
-    # AUTH PROVIDERS
     "db_graph",
     "apollo_people_search",
-    "channels",
     "gmail_subscriptions",
-    "django_object_actions",
     "base_model.apps.BaseModelConfig",
-    "allauth.socialaccount.providers.google",
-    "widget_tweaks",
-    "slippers",
-    "django_extensions",
     "gmail_events",
     "webhook_tester",
     "credentials",
     "base64_utils",
-    "debug_toolbar",
-    # needs to be last
+    # This should be last
     "admin_autoregister",
 ]
 
-ALLAUTH_UI_THEME = "light"
+# Middleware Configuration
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "bluewind.admin_site.admin_login_middleware",
+]
 
-
+# Templates Configuration
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -84,184 +91,13 @@ TEMPLATES = [
     },
 ]
 
-MIDDLEWARE = [
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.security.SecurityMiddleware",
-    ## third party package
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-    "allauth.account.middleware.AccountMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-    "bluewind.admin_site.admin_login_middleware",
-]
-
-ROOT_URLCONF = "bluewind.urls"
-
-STATIC_URL = "/static/"
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-SECRET_KEY = os.environ["SECRET_KEY"]
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ["DEBUG"]
-
-ALLOWED_HOSTS = environ.Env().list("ALLOWED_HOSTS")
-
-WSGI_APPLICATION = "bluewind.wsgi.application"
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
-]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
-USE_I18N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
-GRAPH_MODELS = {
-    "all_applications": True,
-    "group_models": True,
-}
-
-AUTHENTICATION_BACKENDS = [
-    # Needed to login by username in Django admin, regardless of `allauth`
-    "django.contrib.auth.backends.ModelBackend",
-    "allauth.account.auth_backends.AuthenticationBackend",
-    # `allauth` specific authentication methods, such as login by email
-    # 'allauth.account.auth_backends.AuthenticationBackend',
-]
-
-
-SOCIALACCOUNT_PROVIDERS = {
-    "google": {
-        # For each OAuth based provider, either add a ``SocialApp``
-        # (``socialaccount`` app) containing the required client
-        # credentials, or list them here:
-        "APP": {
-            "client_id": os.environ["GOOGLE_OAUTH_CLIENT_ID"],
-            "secret": os.environ["GOOGLE_OAUTH_CLIENT_SECRET"],
-            # 'key': ''
-        }
-    }
-}
-
-
-LOGIN_URL = "/accounts/login/"
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_USERNAME_REQUIRED = False
-ACCOUNT_AUTHENTICATION_METHOD = "email"
-LOGIN_REDIRECT_URL = "/admin"
-ACCOUNT_EMAIL_VERIFICATION = "mandatory"
-SITE_ID = 1
-
-if os.environ["ENVIRONMENT"] == "prod":
-    SITE_URL = os.environ["SITE_URL"]
-else:
-    SITE_URL = "https://green.bluewind.ai"
-
-
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,  # Change this to False
-    "handlers": {
-        "console": {
-            "class": "rich.logging.RichHandler",
-            "rich_tracebacks": True,
-            "tracebacks_show_locals": True,
-        },
-    },
-    "loggers": {
-        "": {  # Root logger
-            "level": "DEBUG",
-            "handlers": ["console"],
-        },
-        "django.db.backends": {
-            "level": "DEBUG",
-            "handlers": ["console"],
-            "propagate": False,  # Add this line
-        },
-    },
-}
-
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
-
-# SESSION_ENGINE = "django.contrib.sessions.middleware.SessionMiddleware"
-
-USE_X_FORWARDED_HOST = True
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
-# Take environment variables from .env file
-# if os.environ['ENVIRONMENT') != 'prod' and os.environ['ENVIRONMENT') != 'staging']
-#     if os.environ['ENVIRONMENT') == 'test']
-#         environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-#         os.environ['ENVIRONMENT'] = 'test'
-#     else:
-#         environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-# GOOGLE_OAUTH_CLIENT_ID = env.list('GOOGLE_OAUTH_CLIENT_ID')
-# GOOGLE_OAUTH_CLIENT_SECRET= env.list('GOOGLE_OAUTH_CLIENT_SECRET')
-
-# Application definition
-
-WSGI_APPLICATION = "bluewind.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+# Database Configuration
 DB_USERNAME = os.environ["DB_USERNAME"]
 DB_PASSWORD = os.environ["DB_PASSWORD"]
 DB_HOST = os.environ["DB_HOST"]
 DB_PORT = os.environ["DB_PORT"]
 DB_NAME = os.environ["DB_NAME"]
 
-# DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/postgres"
 DB_INFO = {
     "ENGINE": "django.db.backends.postgresql",
     "NAME": DB_NAME,
@@ -276,91 +112,112 @@ DB_INFO = {
 }
 DATABASES = {"default": DB_INFO}
 
+# Authentication and Authorization
+AUTH_USER_MODEL = "users.User"
 
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
+
+# Password Validation
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+]
+
+# Internationalization
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Security Settings
 CSRF_TRUSTED_ORIGINS = os.environ["CSRF_TRUSTED_ORIGINS"].split(",")
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
+# AllAuth Settings
+SITE_ID = 1
+LOGIN_URL = "/accounts/login/"
+LOGIN_REDIRECT_URL = "/admin"
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 
-# if 'test' in sys.argv:
-#     DATABASES['default']['USER'] = 'test_user'
-#     DATABASES['default']['PASSWORD'] = 'test_password'
+# Social Account Providers
+SOCIALACCOUNT_PROVIDERS = {
+    "google": {
+        "APP": {
+            "client_id": os.environ["GOOGLE_OAUTH_CLIENT_ID"],
+            "secret": os.environ["GOOGLE_OAUTH_CLIENT_SECRET"],
+        }
+    }
+}
 
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+# Logging Configuration
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "rich.logging.RichHandler",
+            "rich_tracebacks": True,
+            "tracebacks_show_locals": True,
+        },
+    },
+    "loggers": {
+        "": {
+            "level": "DEBUG",
+            "handlers": ["console"],
+        },
+        "django.db.backends": {
+            "level": "DEBUG",
+            "handlers": ["console"],
+            "propagate": False,
+        },
+    },
+}
 
-
+# Debug Toolbar Settings
+INTERNAL_IPS = ["127.0.0.1"]
 DEBUG_TOOLBAR_CONFIG = {
-    # 'EXTRA_SIGNALS': True,
     "SHOW_TEMPLATE_CONTEXT": True,
     "ENABLE_STACKTRACES": True,
-    # 'SHOW_TOOLBAR_CALLBACK': lambda request: True,
-    "STACKTRACE_DEPTH": 10000,  # Increase this number as needed
+    "STACKTRACE_DEPTH": 10000,
     "SHOW_TOOLBAR_CALLBACK": "bluewind.toolbar.show_toolbar",
 }
 
-DEBUG_TOOLBAR_PANELS = [
-    "debug_toolbar.panels.versions.VersionsPanel",
-    "debug_toolbar.panels.timer.TimerPanel",
-    "debug_toolbar.panels.settings.SettingsPanel",
-    "debug_toolbar.panels.headers.HeadersPanel",
-    "debug_toolbar.panels.request.RequestPanel",
-    "debug_toolbar.panels.sql.SQLPanel",
-    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
-    "debug_toolbar.panels.templates.TemplatesPanel",
-    "debug_toolbar.panels.cache.CachePanel",
-    "debug_toolbar.panels.signals.SignalsPanel",
-    "debug_toolbar.panels.logging.LoggingPanel",
-    "debug_toolbar.panels.redirects.RedirectsPanel",
-]
-
-
-# LOGGING = {
-#     'version': 1,
-#     'filters': {
-#         'require_debug_true': {
-#             '()': 'django.utils.log.RequireDebugTrue',
-#         }
-#     },
-#     'handlers': {
-#         'console': {
-#             'level': 'DEBUG',
-#             'filters': ['require_debug_true'],
-#             'class': 'logging.StreamHandler',
-#         }
-#     },
-#     'loggers': {
-#         'django.db.backends': {
-#             'level': 'DEBUG',
-#             'handlers': ['console'],
-#         }
-#     }
-# }
-
-
-# dummy change to test cia
-# cdnsjkcdnsjkcndsk
-
-TEST_RUNNER = "bluewind.test_runner.NoDbTestRunner"
-
-
-AUTH_USER_MODEL = "users.User"
-
-ACCOUNT_ADAPTER = "bluewind.admin_site.CustomAccountAdapter"
-
+# Email Configuration (AWS SES)
 EMAIL_BACKEND = "django_ses.SESBackend"
-
-# These are optional if you are using AWS IAM Roles https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles.html
 AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
-# https://docs.aws.amazon.com/cli/v1/userguide/cli-configure-files.html
-# AWS_SESSION_PROFILE = 'YOUR-PROFILE-NAME'
-# Additionally, if you are not using the default AWS region of us-east-1,
-# you need to specify a region, like so:
 AWS_SES_REGION_NAME = "us-west-2"
 AWS_SES_REGION_ENDPOINT = "email.us-west-2.amazonaws.com"
-
-# If you want to use the SESv2 client
 USE_SES_V2 = True
-
 DEFAULT_FROM_EMAIL = "wayne@bluewind.ai"
 
+# Miscellaneous
 SALT_KEY = os.environ["SALT_KEY"]
+ALLAUTH_UI_THEME = "light"
+TEST_RUNNER = "bluewind.test_runner.NoDbTestRunner"
+GRAPH_MODELS = {
+    "all_applications": True,
+    "group_models": True,
+}
+
+# Environment-specific settings
+if os.environ["ENVIRONMENT"] == "prod":
+    SITE_URL = os.environ["SITE_URL"]
+else:
+    SITE_URL = "https://green.bluewind.ai"
