cf5c5ae0d3602d674be13260bf615cbbc94c885d
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-04 15:47:51 -0700

    checkpoint

--- actions/models.py
+++ actions/models.py
@@ -1,19 +1,29 @@
-from django import forms
+from base_model_admin.admin import InWorkspace
+from django.apps import apps
+from django.contrib import admin
 from django.db import models
 from workspaces.models import WorkspaceRelated
 
 
 class Action(WorkspaceRelated):
-    form = models.CharField(
-        max_length=255
-    )  # This will store the form class name as a string
+    form = models.CharField(max_length=255)  # This stores the model name
 
-    def get_form_class(self):
-        return getattr(forms, self.form)
-
-    def execute(self, form_data):
-        # Implementation of execution logic
-        pass
+    def get_model_and_admin(self):
+        model_name = self.form.replace("ModelForm", "")
+        for app_config in apps.get_app_configs():
+            try:
+                model = app_config.get_model(model_name)
+                model_admin = admin.site._registry[model]
+                return model, model_admin
+            except LookupError:
+                continue
 
     def __str__(self):
         return f"Action: {self.form} for Workspace: {self.workspace}"
+
+
+class ActionAdmin(InWorkspace):
+    def change_view(self, request, object_id, form_url="", extra_context=None):
+        action = self.get_object(request, object_id)
+        _, model_admin = action.get_model_and_admin()
+        return model_admin.add_view(request)
