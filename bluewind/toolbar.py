e8a3fbb7707e84e0af14947cb08f33f31ac53ec1
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-08 17:58:59 -0700

    checkpoint

--- bluewind/toolbar.py
+++ bluewind/toolbar.py
@@ -2,5 +2,5 @@ def show_toolbar(request):
     return (
         "XMLHttpRequest" not in request.headers.get("x-requested-with", "")
         and request.user
-        and request.user.username != "wayne@bluewind.ai"
+        and request.user.username == "wayne@bluewind.ai"
     )
