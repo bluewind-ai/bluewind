06497e5ad291cf07941c385527f32780995cd983
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-09 21:07:47 -0700

    checkpoint

--- flows/tests.py
+++ flows/tests.py
@@ -43,41 +43,34 @@ class SimpleFlowTestCase(TestCase):
         )
 
     def test_create_channel_flow(self):
-        workspace = Workspace.objects.create(name="Test Workspace")
-        user = User.objects.create(username="testuser")
-        flow = Flow.objects.create(name="Create Channel Flow", workspace=workspace)
-        flow_run = FlowRun.objects.create(flow=flow, workspace=workspace)
+        flow_run = FlowRun.objects.create(flow=self.flow, workspace=self.workspace)
 
-        model = Model.objects.get(name="Channel", app_label="channels")
-        action = Action.objects.create(
-            workspace=workspace, action_type=Action.ActionType.CREATE, model=model
+        # First, create StepRun
+        step_run = StepRun.objects.create(
+            step=self.flow_step, workspace=self.workspace, flow_run=flow_run
         )
-        step = Step.objects.create(flow=flow, action=action, workspace=workspace)
 
-        # First, create ActionRun
+        # Now create ActionRun with the StepRun
         action_run = ActionRun.objects.create(
-            action=action,
-            workspace=workspace,
-            user=user,
+            action=self.action,
+            step_run=step_run,
+            workspace=self.workspace,
+            user=self.user,
             model_name="Channel",
             data={},
             action_input={"name": "Test Channel", "description": "A test channel"},
         )
 
-        # Now create StepRun with the ActionRun
-        step_run = StepRun.objects.create(
-            step=step, workspace=workspace, action_run=action_run
-        )
+        # Update StepRun with the created ActionRun
+        step_run.action_run = action_run
+        step_run.save()
 
-        # Update ActionRun with the created StepRun
-        action_run.step_run = step_run
-        action_run.save()
-
-        self.assertEqual(action_run.action, action)
+        self.assertEqual(action_run.action, self.action)
         self.assertEqual(action_run.step_run, step_run)
         self.assertEqual(step_run.action_run, action_run)
-        self.assertEqual(action_run.workspace, workspace)
-        self.assertEqual(action_run.user, user)
+        self.assertEqual(step_run.flow_run, flow_run)
+        self.assertEqual(action_run.workspace, self.workspace)
+        self.assertEqual(action_run.user, self.user)
         self.assertEqual(action_run.model_name, "Channel")
         self.assertEqual(action_run.action_input["name"], "Test Channel")
         self.assertEqual(action_run.action_input["description"], "A test channel")
