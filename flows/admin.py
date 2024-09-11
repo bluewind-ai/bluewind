d85ea28689acf4f5a78a7acfc698cc2928b090fa
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-11 14:45:22 -0700

    checkpoint

--- flows/admin.py
+++ flows/admin.py
@@ -1,8 +1,24 @@
+from django_json_widget.widgets import JSONEditorWidget
 from django_reverse_admin import ReverseModelAdmin
 
 from base_model_admin.admin import InWorkspace
+from django import forms
 from django.contrib import admin
 from flows.models import FlowRun
+from workspace_snapshots.models import WorkspaceDiff
+
+
+class WorkspaceDiffForm(forms.ModelForm):
+    class Meta:
+        model = WorkspaceDiff
+        fields = "__all__"
+        widgets = {"diff_data": JSONEditorWidget()}
+
+
+class WorkspaceDiffInline(admin.StackedInline):
+    model = WorkspaceDiff
+    form = WorkspaceDiffForm
+    fk_name = "flow_runs"  # Adjust this if the related_name is different
 
 
 class FlowRunAdmin(ReverseModelAdmin, InWorkspace):
@@ -10,7 +26,7 @@ class FlowRunAdmin(ReverseModelAdmin, InWorkspace):
     inline_reverse = [
         (
             "diff",
-            {},
+            {"form": WorkspaceDiffForm, "admin_class": WorkspaceDiffInline},
         ),
     ]
 
--- flows/flows/create_basic_flow.py
+++ flows/flows/create_basic_flow.py
@@ -1,7 +1,7 @@
 from credentials.models import Credentials
 from django.contrib.auth import get_user_model
 from django.utils import timezone
-from flows.models import Action, Flow, FlowRun, Step
+from flows.models import Action, ActionRun, Flow, FlowRun, Model, Step, StepRun
 
 
 def create_basic_flow(workspace):
@@ -14,7 +14,7 @@ def create_basic_flow(workspace):
     setup_flow = Flow.objects.create(
         workspace=workspace,
         name="Setup Test Environment",
-        type="no-code",  # Set this to 'no-code' to prevent recursion
+        type="no-code",
     )
 
     # Create and start the flow run
@@ -31,24 +31,38 @@ def create_basic_flow(workspace):
         workspace=workspace, key="TEST_CREDENTIAL", value="test_value"
     )
 
-    # Get or create the action
-    action, _ = Action.objects.get_or_create(
+    # Get the action
+    model = Model.objects.get(workspace=workspace, name="credentials")
+    action = Action.objects.get(
         workspace=workspace,
         action_type=Action.ActionType.CREATE,
-        model__name="credentials",
+        model=model,
     )
 
-    # Create test flow
-    test_flow = Flow.objects.create(workspace=workspace, name="Test Flow")
-
     # Create step
-    step = Step.objects.create(workspace=workspace, flow=test_flow, action=action)
+    step = Step.objects.create(workspace=workspace, flow=setup_flow, action=action)
+
+    # Create step run
+    step_run = StepRun.objects.create(workspace=workspace, flow_run=new_flow_run)
+
+    # Create action run
+    action_run = ActionRun.objects.create(
+        workspace=workspace,
+        action=action,
+        step_run=step_run,
+        user=user,
+        model_name=action.model.full_name,
+        status="COMPLETED",
+    )
 
     # Update flow run state and status
     new_flow_run.state = {
         "created_credential_id": credential.id,
-        "created_flow_id": test_flow.id,
         "created_step_id": step.id,
+        "created_step_run_id": step_run.id,
+        "created_action_run_id": action_run.id,
     }
     new_flow_run.status = FlowRun.Status.COMPLETED
     new_flow_run.save()
+
+    return new_flow_run
