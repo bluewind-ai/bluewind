bdbbc2e85d5056fdb38003a1cc95aa368b2258d9
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-11 14:33:25 -0700

    checkpoint

--- base_model/models.py
+++ base_model/models.py
@@ -1,9 +1,12 @@
 # models.py
 import logging
 
+from django_json_widget.widgets import JSONEditorWidget
 from model_clone import CloneMixin
 
+from django import forms
 from django.db import models
+from django.db.models import JSONField
 
 logger = logging.getLogger(__name__)
 
@@ -19,3 +22,14 @@ class BaseModel(CloneMixin, models.Model):
     _clone_excluded_fields = [
         "id",
     ]
+
+
+class BaseModelForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(self.instance._meta.get_field(field_name), JSONField):
+                self.fields[field_name].widget = JSONEditorWidget()
+
+    class Meta:
+        abstract = True
