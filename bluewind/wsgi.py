9847faf82f9902883c26dc33d0ac1d73ee1eb3f0
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-11 22:04:05 -0700

    checkpoint

--- bluewind/wsgi.py
+++ bluewind/wsgi.py
@@ -5,24 +5,19 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bluewind.settings_prod")
 
 
-def workspace_wsgi_middleware(application):
+def workspace_wsgi_middleware(django_app):
     def wrapper(environ, start_response):
         path_info = environ.get("PATH_INFO", "")
         parts = path_info.split("/")
-        # if parts[2].startswith('accounts'):
-        #         pass
         if path_info.startswith("/static"):
             pass
         elif path_info.startswith("/workspaces/"):
             parts = path_info.split("/")
             workspace_id = parts[2]
-            # strip out the 'workspaces/' prefix
-            # Modify SCRIPT_NAME and PATH_INFO
             environ["SCRIPT_NAME"] = f"/workspaces/{workspace_id}"
             environ["PATH_INFO"] = "/" + "/".join(parts[3:])
-
             environ["WORKSPACE_ID"] = workspace_id
-        return application(environ, start_response)
+        return django_app(environ, start_response)
 
     return wrapper
 
