5b457bf91df4f7fb1576e7825145b108a2e3b87e
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-12 13:39:51 -0700

    Update pg_sync.sh

--- pg_sync.sh
+++ pg_sync.sh
@@ -1,5 +1,8 @@
 # #!/bin/bash
 
+PGPASSWORD=$DB_PASSWORD dropdb -h $DB_HOST -U $DB_USERNAME $DB_NAME
+PGPASSWORD=$DB_PASSWORD createdb -h $DB_HOST -U $DB_USERNAME $DB_NAME
+
 # # Run pg_dump on the replica
 
 # # PGPASSWORD=$REPLICA_PASSWORD /opt/homebrew/opt/postgresql@16/bin/pg_dump -h $REPLICA_HOST -U $REPLICA_USER -d $REPLICA_DB >backup.sql
@@ -9,8 +12,6 @@
 
 # PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USERNAME -d postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '$DB_NAME';"
 
-PGPASSWORD=$DB_PASSWORD dropdb -h $DB_HOST -U $DB_USERNAME $DB_NAME
-PGPASSWORD=$DB_PASSWORD createdb -h $DB_HOST -U $DB_USERNAME $DB_NAME
 # PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USERNAME -d $DB_NAME <backup.sql
 
 # # Clean up
