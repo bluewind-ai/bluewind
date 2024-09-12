9c68c17875673e3c45272bfc37c71712c4b5943d
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-11 21:31:16 -0700

    Update models.py

--- flows/models.py
+++ flows/models.py
@@ -49,6 +49,9 @@ class FlowRun(WorkspaceRelated):
     )
     create_new_workspace = models.BooleanField(default=False)
 
+    class Meta:
+        unique_together = ["name", "workspace"]
+
     def __str__(self):
         return f"Run of {self.flow.name} at {self.created_at}"
 
