5340e18f0154343ffdc0d24c626f17668b9f4bfb
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-14 22:55:28 -0700

    checkpoint

--- bluewind/urls.py
+++ bluewind/urls.py
@@ -7,7 +7,6 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
 
 from bluewind.admin_site import custom_admin_site
-from bluewind.auto_api import generate_auto_api
 from channels.models import oauth2callback
 from health_check.views import health_check
 
@@ -34,7 +33,6 @@ urlpatterns = [
     path("oauth2callback/", oauth2callback, name="oauth2callback"),
     path("accounts/", include("allauth.urls")),
     # DRF and DRF Spectacular paths
-    path("api/", include(generate_auto_api())),  # Dynamically include all API paths
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/schema/swagger-ui/",
