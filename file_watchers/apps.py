3a3809bbf9383599b2f9c5fa3855d48a22a304c1
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-15 13:54:31 -0700

    checkpoint

--- file_watchers/apps.py
+++ file_watchers/apps.py
@@ -6,3 +6,8 @@ from django.apps import AppConfig
 class FileWatchersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "file_watchers"
+
+    def ready(self):
+        from flows.flows.file_watchers_on_ready import file_watchers_on_ready
+
+        file_watchers_on_ready()
