e38dcf768e77fe64b5b591bac2b0b168dc4ffd00
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-14 23:04:46 -0700

    checkpoint

--- flows/steps.py
+++ flows/steps.py
@@ -1,38 +1,10 @@
 import logging
 
-from django.contrib.contenttypes.models import ContentType
-from django.core.serializers.json import DjangoJSONEncoder
-from django.db import models, transaction
-from django.forms import ValidationError
+from django.db import models
 
-from credentials.models import Credentials
-from flows.recordings import Recording
-from flows.step_runs import StepRun
 from workspaces.models import WorkspaceRelated
 
 logger = logging.getLogger(__name__)
-
-logger = logging.getLogger(__name__)
-
-
-class Action(WorkspaceRelated):
-    class ActionType(models.TextChoices):
-        CREATE = "CREATE", "Create"
-        SAVE = "SAVE", "Save"
-        DELETE = "DELETE", "Delete"
-        CUSTOM = "CUSTOM", "Custom"
-        LIST = "LIST", "List"
-        SHOW = "SHOW", "Show"
-
-    action_type = models.CharField(max_length=20, choices=ActionType.choices)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    is_recorded = models.BooleanField(default=True)
-
-    def __str__(self):
-        return f"{self.content_type.model} {self.get_action_type_display()}"
-
-    class Meta:
-        unique_together = ("workspace", "action_type", "content_type")
 
 
 class Step(WorkspaceRelated):
