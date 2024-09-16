92d44500dbbff93e463d3608a5aa4373d8e033b0
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-16 12:19:35 -0700

    fork django object actions

---
+++ django_object_actions/__init__.py
@@ -0,0 +1,20 @@
+"""A Django app for adding object tools for models in the admin."""
+
+__version__ = "4.3.0"
+
+
+# kind of like __all__, make these available for public
+from .utils import (
+    BaseDjangoObjectActions,
+    DjangoObjectActions,
+    takes_instance_or_queryset,
+    action,
+)
+
+
+__all__ = [
+    "BaseDjangoObjectActions",
+    "DjangoObjectActions",
+    "takes_instance_or_queryset",
+    "action",
+]
