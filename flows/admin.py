fb093a65e209174393c15b3ded81d4ae5f79c894
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-16 11:08:57 -0700

    checkpoint

---
+++ flows/admin.py
@@ -0,0 +1,22 @@
+from django_object_actions import DjangoObjectActions
+
+from base_model_admin.admin import InWorkspace
+from flows.flows.flow_runs_create_form import flow_runs_create_form
+
+
+class FlowAdmin(DjangoObjectActions, InWorkspace):
+    add_form_template = "admin/flow_runs/flowrun/add_form.html"
+
+    change_actions = ("run_flow",)
+
+    def run_flow(self, request, obj):
+        # Implement your custom action logic here
+        # For example:
+        # Do something with the flow object
+        context = self.admin_site.each_context(request)
+        return flow_runs_create_form(
+            request, obj, "admin/flow_runs/flowrun/add_form.html", context
+        )
+
+    run_flow.label = "Run Flow"
+    run_flow.short_description = "Perform a custom action on this Flow"
--- flows/flows/flow_runs_create_form.py
+++ flows/flows/flow_runs_create_form.py
@@ -4,14 +4,13 @@ from django.template.response import TemplateResponse
 
 
 def flow_runs_create_form(request, flow, add_form_template, context, form=None):
-    if form is None:
-        module_name = f"flows.flows.{flow.name}"
-        flow_module = importlib.import_module(module_name)
-        function_name = flow.name
-        snake_function_name = "".join(word.title() for word in function_name.split("_"))
-        form_class_name = f"{snake_function_name}Form"
-        FormClass = getattr(flow_module, form_class_name)
-        form = FormClass()
+    module_name = f"flows.flows.{flow.name}"
+    flow_module = importlib.import_module(module_name)
+    function_name = flow.name
+    snake_function_name = "".join(word.title() for word in function_name.split("_"))
+    form_class_name = f"{snake_function_name}Form"
+    FormClass = getattr(flow_module, form_class_name)
+    form = FormClass()
 
     context = {
         **context,
