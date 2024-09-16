0d263faa4f2a08bfd7c0474af7064a1e05aa5eb7
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-16 12:34:37 -0700

    checkpoint

--- flows/admin.py
+++ flows/admin.py
@@ -1,12 +1,9 @@
-from django_object_actions import DjangoObjectActions
-
 from base_model_admin.admin import InWorkspace
+from django_object_actions import DjangoObjectActions
 from flows.flows.flow_runs_create_form import flow_runs_create_form
 
 
 class FlowAdmin(DjangoObjectActions, InWorkspace):
-    add_form_template = "admin/flow_runs/flowrun/add_form.html"
-
     change_actions = ("run_flow",)
 
     def run_flow(self, request, obj):
