7e68fe7c726c37ebee67665dbc00b72e697b6b1f
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-16 09:20:42 -0700

    Update admin.py

--- models/admin.py
+++ models/admin.py
@@ -1,3 +1,14 @@
+# admin.py
+
 from django.contrib import admin
 
-# Register your models here.
+from base_model_admin.admin import InWorkspace
+
+from .models import Model
+
+
+@admin.register(Model)
+class ModelAdmin(InWorkspace):
+    list_display = ("name", "app_label", "workspace", "file")
+    list_filter = ("app_label", "workspace")
+    search_fields = ("name", "app_label", "file__path")
