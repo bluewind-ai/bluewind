b8257e95b24bdf97b6f6293e152442b92981b324
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-16 17:55:32 -0700

    checkpoint

--- flows/flows/flows_update_arguments.py
+++ flows/flows/flows_update_arguments.py
@@ -1,9 +1,12 @@
 import logging
 
+from flows.flows.get_function_parameters import get_function_parameters
 from flows.flows.models_update_after_file_changes import flows_update_arguments
 
 logger = logging.getLogger("django.debug")
 
 
 def flows_update_arguments(file_change):
+    parameters = get_function_parameters(file_change)
+
     pass
