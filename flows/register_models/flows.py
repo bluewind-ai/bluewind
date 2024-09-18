594fb7acd767bfd0ea0680884d049974f8ee1d54
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-17 17:24:57 -0700

    Update flows.py

--- flows/register_models/flows.py
+++ flows/register_models/flows.py
@@ -2,33 +2,40 @@ import logging
 import os
 
 from django.apps import apps
+from django.db import IntegrityError
 
 from models.models import Model
 
-logger = logging.getLogger("django.not_used")
+logger = logging.getLogger("django.temp")
 
 
 def register_models(base_dir, default_workspace_id, default_user_id, all_files):
-    models_to_create = []
+    models_created = []
     file_dict = {file.path: file for file in all_files}
-    registered_models = set(Model.objects.values_list("name", flat=True))
+    existing_models = {model.file_id: model for model in Model.objects.all()}
 
     for app_config in apps.get_app_configs():
-        for model in app_config.get_models():
-            model_name = model.__name__
-            app_label = app_config.label
-            file_path = os.path.join(base_dir, app_label, "models.py")
+        app_label = app_config.label
+        file_path = os.path.join(base_dir, app_label, "models.py")
 
-            if model_name not in registered_models and file_path in file_dict:
-                file_instance = file_dict[file_path]
+        if file_path in file_dict:
+            file_instance = file_dict[file_path]
+
+            if file_instance.id not in existing_models:
                 model_instance = Model(
                     workspace_id=default_workspace_id,
-                    name=model_name,
                     app_label=app_label,
                     user_id=default_user_id,
                     file=file_instance,
                 )
-                models_to_create.append(model_instance)
+                try:
+                    model_instance.save()
+                    models_created.append(model_instance)
+                except IntegrityError:
+                    logger.warning(
+                        f"Model for file {file_path} already exists. Skipping."
+                    )
+            else:
+                logger.info(f"Model for file {file_path} already exists. Skipping.")
 
-    Model.objects.bulk_create(models_to_create)
-    return models_to_create
+    return models_created
