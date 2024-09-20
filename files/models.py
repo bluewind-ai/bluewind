8eeacae84e4308dc7cf5bb0da37265ae629529c2
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-19 17:42:30 -0700

    checkpoint

--- files/models.py
+++ files/models.py
@@ -5,7 +5,7 @@ from workspaces.models import WorkspaceRelated
 
 
 class File(WorkspaceRelated):
-    path = models.CharField(max_length=255, unique=True, help_text="The file path.")
+    path = models.CharField(max_length=255, help_text="The file path.")
     content = models.TextField(help_text="The content of the file.")
     created_at = models.DateTimeField(
         auto_now_add=True, help_text="Timestamp when the file was created."
@@ -25,6 +25,4 @@ class File(WorkspaceRelated):
 
     class Meta:
         ordering = ["-updated_at"]
-
-
-"cdcds"
+        unique_together = ["workspace", "path"]
