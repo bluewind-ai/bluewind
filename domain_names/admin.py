f7d898c241a3e40c5beb0919c790f5bdfaedd870
Merwane Hamadi <merwanehamadi@gmail.com>
2024-09-27 11:24:26 -0700

    checkpoint

--- domain_names/admin.py
+++ domain_names/admin.py
@@ -1,2 +1,9 @@
-
 # Register your models here.
+
+from base_model_admin.admin import InWorkspace
+
+
+class DomainNameAdmin(InWorkspace):
+    list_display = ("name", "workspace")
+    search_fields = ("name",)
+    list_filter = ("workspace",)
