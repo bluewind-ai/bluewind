78fdb4b04ffc260da4e7c7428dc48f060d57f39d
Merwane Hamadi <merwanehamadi@gmail.com>
2024-10-05 19:22:35 -0700

    checkpoint

--- functions/handle_function_call_after_save/v1/functions.py
+++ functions/handle_function_call_after_save/v1/functions.py
@@ -2,7 +2,6 @@ import logging
 
 from django.utils import timezone
 
-from domain_names.models import DomainName
 from function_calls.models import FunctionCall
 from functions.bluewind_function.v1.functions import custom_serialize
 from functions.handle_mark_function_call_as_successful.v1.functions import (
@@ -14,7 +13,8 @@ logger = logging.getLogger("django.not_used")
 
 def handle_function_call_after_save_v1(object):
     object.function_call.status = FunctionCall.Status.COMPLETED
-    query_set = DomainName.objects.filter(id=object.id)
+    model_class = type(object)
+    query_set = model_class.objects.filter(id=object.id)
     serialized_data = custom_serialize(query_set)
     object.function_call.output_data = serialized_data
     object.function_call.executed_at = timezone.now()
