c66c1e15209dab369885dac92099d4cddd88b28f
Merwane Hamadi <merwanehamadi@gmail.com>
2024-10-06 19:14:38 -0700

    Update functions.py

--- functions/replay_everything/v1/functions.py
+++ functions/replay_everything/v1/functions.py
@@ -5,9 +5,12 @@ from functions.reset.v1.functions import reset_v1
 from functions.run_until_complete.v1.functions import run_until_complete_v1
 
 logger = logging.getLogger("django.not_used")
+"cdscds"
 
 
 def replay_everything_v1(user):
     reset_v1(user=user)
     function_call, _ = master_v1()
-    return run_until_complete_v1(function_call=function_call, user=user)
+    return run_until_complete_v1(
+        function_call=function_call, user=user, function_call_to_complete=function_call
+    )
