819698052c4ee0ce3f8f4a2c6d34f49a00bd3433
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-05 14:30:12 -0800

    checkpoint

--- app/routes/action-calls.$id/route.tsx
+++ app/routes/action-calls.$id/route.tsx
@@ -1,25 +1,17 @@
 // app/routes/action-calls.$id/route.tsx
 
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, useRouteError } from "@remix-run/react";
 import { json } from "@remix-run/node";
-import type { LoaderFunction } from "@remix-run/node";
+import type { LoaderFunction, LoaderArgs } from "@remix-run/node";
 import { db } from "~/db";
 import { actionCalls } from "~/db/schema";
 import { eq } from "drizzle-orm";
 
-export const loader: LoaderFunction = async ({ params }) => {
+export const loader: LoaderFunction = async ({ params }: LoaderArgs) => {
   dd("ID Route Loader", { params });
-  console.log("=== ID ROUTE LOADER START ===", { params });
-
-  if (!params.id) {
-    console.log("No ID found in params");
-    return json({ error: "Missing ID" }, { status: 404 });
-  }
-
-  console.log(`Looking up action call with ID: ${params.id}`);
 
   const actionCall = await db.query.actionCalls.findFirst({
-    where: eq(actionCalls.id, parseInt(params.id)),
+    where: eq(actionCalls.id, parseInt(params.id)), // TypeScript should know id exists here
     with: {
       action: true,
     },
@@ -34,13 +26,3 @@ export const loader: LoaderFunction = async ({ params }) => {
     data: actionCall,
   });
 };
-
-export default function Route() {
-  const data = useLoaderData<typeof loader>();
-
-  return (
-    <main className="flex-1 bg-black text-green-400 p-4 font-mono">
-      <pre className="whitespace-pre-wrap">{JSON.stringify(data, null, 2)}</pre>
-    </main>
-  );
-}
