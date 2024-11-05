b11bf9b5e8ddbc57e9fdf6e0780e3bb063f8e3f2
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-05 09:57:04 -0800

    checkpoint

--- app/components/icons/logo.tsx
+++ app/components/icons/logo.tsx
@@ -1,5 +1,5 @@
 // app/components/icons/logo.tsx
 
 export function Logo() {
-  return <img src="/icons/bluewind.icon" alt="BlueWind Logo" className="h-5 w-5" />;
+  return <img src="/icons/favicon.icon" alt="BlueWind Logo" className="h-5 w-5" />;
 }
