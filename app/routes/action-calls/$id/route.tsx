bd6b5e60ae4b10fa072d337aecffb5846468253c
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-05 13:17:42 -0800

    checkpoint

--- app/routes/action-calls/$id/route.tsx
+++ app/routes/action-calls/$id/route.tsx
@@ -1,49 +1,21 @@
 // app/routes/action-calls/$id/route.tsx
 
-import { useLoaderData } from "@remix-run/react";
-import { ActionCallTree } from "~/components/ui/ActionCallTree";
-import { GoNextButton } from "~/components/GoNextButton";
-import { useState } from "react";
-import { ActionCallDetails } from "~/components/ActionCallDetails";
-import { DebugPanel } from "~/components/DebugPanel";
-import { ActivityBar } from "~/components/activity-bar";
-import type { loader } from "./loader";
-import type { actionCalls } from "~/db/schema";
-import type { InferSelectModel } from "drizzle-orm";
+export function ErrorBoundary() {
+  const error = useRouteError();
 
-export { loader } from "./loader";
-export { action } from "./action";
-
-type ActionCall = InferSelectModel<typeof actionCalls>;
-
-function ActionSection({ actionCall }: { actionCall: ActionCall }) {
-  const [debugMessage, setDebugMessage] = useState<string | null>(null);
-
-  return (
-    <div className="flex-1 relative">
-      <div className="absolute top-4 left-1/2 -translate-x-1/2 z-50">
-        <GoNextButton actionCall={actionCall} onDebugData={setDebugMessage} />
-      </div>
-      <DebugPanel debugMessage={debugMessage} />
-    </div>
-  );
-}
-
-export default function ActionCallPage() {
-  const { result, treeData } = useLoaderData<typeof loader>();
+  let errorContent;
+  try {
+    errorContent = typeof error.data === "string" ? JSON.parse(error.data) : error.data;
+  } catch {
+    errorContent = error.data || error;
+  }
 
   return (
     <div className="flex min-h-screen">
       <ActivityBar />
-      <div className="flex flex-1">
-        <div className="w-64">
-          <ActionCallTree initialTreeData={treeData} />
-        </div>
-        <div className="flex-1 p-4 flex flex-col relative">
-          <ActionCallDetails actionCall={result.actionCall} action={result.action} />
-          <ActionSection actionCall={result.actionCall} />
-        </div>
-      </div>
+      <main className="flex-1 bg-black text-green-400 p-4 font-mono">
+        <pre className="whitespace-pre-wrap">{JSON.stringify(errorContent, null, 2)}</pre>
+      </main>
     </div>
   );
 }
