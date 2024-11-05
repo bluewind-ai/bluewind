590e82a8ac45837b46c25a51b03ce902d2dc1f8a
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-05 13:35:53 -0800

    checkpoint

--- app/routes/action-calls.$id/route.tsx
+++ app/routes/action-calls.$id/route.tsx
@@ -1,14 +1,8 @@
 // app/routes/action-calls.$id/route.tsx
 
-import { useLoaderData, useRouteError } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import { ActivityBar } from "~/components/activity-bar";
-import type { loader } from "./loader.server"; // Updated this line
-
-interface RouteError {
-  type: string;
-  message: string;
-  data: unknown;
-}
+import type { loader } from "./loader.server";
 
 export default function Route() {
   const data = useLoaderData<typeof loader>();
@@ -18,29 +12,6 @@ export default function Route() {
       <ActivityBar />
       <main className="flex-1 bg-black text-green-400 p-4 font-mono">
         <pre className="whitespace-pre-wrap">{JSON.stringify(data, null, 2)}</pre>
-      </main>
-    </div>
-  );
-}
-
-export function ErrorBoundary() {
-  const error = useRouteError() as RouteError;
-
-  return (
-    <div className="flex min-h-screen">
-      <ActivityBar />
-      <main className="flex-1 bg-black text-green-400 p-4 font-mono">
-        <pre className="whitespace-pre-wrap">
-          {JSON.stringify(
-            {
-              type: "Debug",
-              message: "Debug Dump",
-              data: error,
-            },
-            null,
-            2,
-          )}
-        </pre>
       </main>
     </div>
   );
