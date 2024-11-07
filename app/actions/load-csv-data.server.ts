85a20d132816b5f90f94cbd98d2616ee276a8ffc
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-06 23:04:20 -0800

    checkpoint

--- app/actions/load-csv-data.server.ts
+++ app/actions/load-csv-data.server.ts
@@ -1,9 +1,9 @@
 // app/actions/load-csv-data.server.ts
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { ActionFunctionArgs } from "@remix-run/node";
 
-import { withActionMiddleware } from "~/lib/action-middleware.server";
-
-export const loadCsvData = withActionMiddleware(async function loadCsvData() {
+export async function loadCsvData(_args: ActionFunctionArgs) {
   void 0; // this should be removed by YOU when you rewrite the file
   // CSV loading logic here
   return true;
-});
+}
