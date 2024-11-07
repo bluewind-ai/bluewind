5e59bdb09ea7e7097b4908c91b4d49ea7548baa4
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-07 12:47:33 -0800

    checkpoint

--- app/lib/generated/actions.ts
+++ app/lib/generated/actions.ts
@@ -4,11 +4,9 @@
 import { master } from "~/actions/master.server";
 import { loadCsvData } from "~/actions/load-csv-data.server";
 import { loadActions } from "~/actions/load-actions.server";
-import { goNext } from "~/actions/go-next.server";
 
 export const actions = {
   master: master,
   "load-csv-data": loadCsvData,
   "load-actions": loadActions,
-  "go-next": goNext,
 } as const;
