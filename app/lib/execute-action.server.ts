914d723c7f88ca4cac4011ae3a5dab29447e5375
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-07 13:15:36 -0800

    Update files from Claude suggestions

--- app/lib/execute-action.server.ts
+++ app/lib/execute-action.server.ts
@@ -2,7 +2,7 @@
 
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { db } from "~/db";
-import { actionCalls } from "~/db/schema";
+import { actions, actionCalls } from "~/db/schema";
 import { actions as actionMap } from "./generated/actions";
 import { contextStore } from "./action-builder.server";
 import type { ActionInsert } from "./action-builder.server";
