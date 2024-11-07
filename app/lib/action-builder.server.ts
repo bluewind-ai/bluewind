ebe4e60f13cafb74c4375e5b7884debd6544f0b3
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-07 12:51:47 -0800

    checkpoint

--- app/lib/action-builder.server.ts
+++ app/lib/action-builder.server.ts
@@ -1,5 +1,5 @@
 // app/lib/action-builder.server.ts
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { withActionMiddleware } from "./action-middleware.server";
 
 export function createAction(name: string, fn: () => Promise<any>) {
