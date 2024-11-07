22a077faaf466624234914c87b644577f8d8a61a
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-07 08:52:13 -0800

    Update files from Claude suggestions

--- app/actions/master.server.ts
+++ app/actions/master.server.ts
@@ -1,36 +1,9 @@
 // app/actions/master.server.ts
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 import type { ActionFunctionArgs } from "@remix-run/node";
-import { db } from "~/db";
-import { actions, actionCalls } from "~/db/schema";
+import { loadCsvData } from "./load-csv-data.server";
 
-export async function master(_args: ActionFunctionArgs) {
-  // First get or create the load-csv-data action
-  const existingAction = await db.query.actions.findFirst({
-    where: (fields, { eq }) => eq(fields.name, "load-csv-data"),
-  });
-
-  const loadCsvAction =
-    existingAction ||
-    (
-      await db
-        .insert(actions)
-        .values({
-          name: "load-csv-data",
-        })
-        .returning()
-    )[0];
-
-  // Create an action call for approval
-  const actionCall = await db
-    .insert(actionCalls)
-    .values({
-      actionId: loadCsvAction.id,
-      status: "ready_for_approval",
-    })
-    .returning();
-
-  return actionCall[0];
+export async function master(args: ActionFunctionArgs) {
+  // Just call the CSV loading function
+  return await loadCsvData(args);
 }
