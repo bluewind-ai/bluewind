9a272640cae56a307338f25b1a34f8ce0f013409
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-07 11:49:35 -0800

    Update files from Claude suggestions

--- app/actions/load-csv-data.server.ts
+++ app/actions/load-csv-data.server.ts
@@ -1,7 +1,5 @@
 // app/actions/load-csv-data.server.ts
 
-import { withActionMiddleware } from "~/lib/action-middleware.server";
-
-export const loadCsvData = withActionMiddleware("load-csv-data", async () => {
+export async function loadCsvData() {
   return true;
-});
+}
--- app/actions/master.server.ts
+++ app/actions/master.server.ts
@@ -1,13 +1,8 @@
 // app/actions/master.server.ts
 
-import { withActionMiddleware } from "~/lib/action-middleware.server";
 import { loadCsvData } from "./load-csv-data.server";
 
-async function masterAction() {
-  // This await is where we'll intercept on hitCount = 2
+export async function master() {
   const csvData = await loadCsvData();
-
   return csvData;
 }
-
-export const master = withActionMiddleware("master", masterAction);
