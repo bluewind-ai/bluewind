20b804627cbabd6a6533f242d455e289c8f1f1ca
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-06 23:07:34 -0800

    checkpoint

--- app/actions/master.server.ts
+++ app/actions/master.server.ts
@@ -1,24 +1,25 @@
 // app/actions/master.server.ts
+
 /* eslint-disable @typescript-eslint/no-unused-vars */
+
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { db } from "~/db";
 import { actions, actionCalls } from "~/db/schema";
 
 export async function master(_args: ActionFunctionArgs) {
-  // Find or create the loadCsvData action
-  const loadCsvAction = await db.query.actions.findFirst({
-    where: (fields, { eq }) => eq(fields.name, "load-csv-data"),
-  });
-
-  if (!loadCsvAction) {
-    throw new Error("load-csv-data action not found");
-  }
+  // First ensure the master action itself is created
+  const loadCsvAction = await db
+    .insert(actions)
+    .values({
+      name: "load-csv-data",
+    })
+    .returning();
 
   // Create an action call for approval
   const actionCall = await db
     .insert(actionCalls)
     .values({
-      actionId: loadCsvAction.id,
+      actionId: loadCsvAction[0].id,
       status: "ready_for_approval",
     })
     .returning();
