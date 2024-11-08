bf3ab0d2231cd5acef3ac8ad42de57204c7dc612
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-07 18:59:47 -0800

    checkpoint

--- app/lib/generated/actions.ts
+++ app/lib/generated/actions.ts
@@ -4,9 +4,11 @@
 import { master } from "~/actions/master.server";
 import { loadCsvData } from "~/actions/load-csv-data.server";
 import { loadActions } from "~/actions/load-actions.server";
+import { getActionCallTree } from "~/actions/get-action-call-tree.server";
 
 export const actions = {
   master: master,
   "load-csv-data": loadCsvData,
   "load-actions": loadActions,
+  "get-action-call-tree": getActionCallTree,
 } as const;
