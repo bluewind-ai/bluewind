3ad082717f5e05b47b51af6af0b99b9bb861c5cc
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-08 12:40:22 -0800

    Update files from Claude suggestions

--- app/routes/objects+/_root.tsx
+++ app/routes/objects+/_root.tsx
@@ -1,16 +1,15 @@
 // app/routes/objects+/_root.tsx
 
-import * as React from "react";
+import * as React from 'react';
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { getTables } from "~/actions/get-tables.server";
-import { NavigationTree } from "~/components/NavigationTree";
-import { Network } from "lucide-react";
+import { NavigationTree } from "~/components/NavigationTree"; // We'll create this
 
 type NavigationNode = {
   id: number;
   name: string;
-  icon?: React.ReactNode | string;
+  icon?: React.ReactNode;
   type: "root" | "app" | "file";
   children: NavigationNode[];
 };
@@ -28,7 +27,7 @@ export async function loader() {
         id: 1,
         name: "Database",
         type: "app",
-        icon: <Network className="h-5 w-5" />,
+        icon: /* Network icon */,
         children: tables.map((tableName, index) => ({
           id: index + 2,
           name: tableName,
