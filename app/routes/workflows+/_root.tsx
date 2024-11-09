984a7eae453df8291f0cd77cadeb3fb67615447d
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-08 20:51:52 -0800

    checkpoint

---
+++ app/routes/workflows+/_root.tsx
@@ -0,0 +1,43 @@
+// app/routes/back-office+/_root.tsx
+
+import { json, type LoaderFunctionArgs } from "@remix-run/node";
+import { Outlet, useLoaderData } from "@remix-run/react";
+import { NavigationTree, type NavigationNode } from "~/components/NavigationTree";
+import { getTableMetadata, apps } from "~/db/schema";
+import { db } from "~/db";
+
+export async function loader({ request: _request }: LoaderFunctionArgs) {
+  const navigationData: NavigationNode = {
+    id: 0,
+    name: "Database",
+    type: "root",
+    iconKey: "database",
+    children: getTableMetadata().map((table, index) => ({
+      id: index + 1,
+      name: table.displayName,
+      urlName: table.urlName,
+      type: "file" as const,
+      children: [] as NavigationNode[],
+    })),
+  };
+
+  const appsData = await db.select().from(apps).orderBy(apps.order);
+
+  return json({
+    navigationData,
+    apps: appsData,
+  });
+}
+
+export default function ObjectsRoot() {
+  const { navigationData, apps } = useLoaderData<typeof loader>();
+
+  return (
+    <div className="flex h-full">
+      <NavigationTree data={navigationData as NavigationNode} apps={apps} />
+      <div className="flex-1">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
