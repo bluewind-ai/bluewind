e119262f3e9e174a753a6b5994dd86ee491baab8
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-08 20:30:59 -0800

    fixes

--- app/components/ViewSelector.tsx
+++ app/components/ViewSelector.tsx
@@ -38,55 +38,51 @@ export function ViewSelector() {
   const selectedView = views.find((view) => view.value === value);
 
   return (
-    <div className="group">
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            role="combobox"
-            aria-expanded={open}
-            className="w-full justify-between"
-          >
-            <div className="flex items-center">
-              {selectedView?.icon}
-              {selectedView?.label || "Select view..."}
-            </div>
-            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-          </Button>
-        </PopoverTrigger>
-        <div className="opacity-50 group-hover:opacity-100 transition-opacity">
-          <PopoverContent className="w-full p-0">
-            <Command>
-              <CommandInput placeholder="Search view..." />
-              <CommandList>
-                <CommandEmpty>No view found.</CommandEmpty>
-                <CommandGroup>
-                  {views.map((view) => (
-                    <CommandItem
-                      key={view.value}
-                      value={view.value}
-                      onSelect={(currentValue) => {
-                        setValue(currentValue);
-                        navigate(`/${currentValue}`);
-                        setOpen(false);
-                      }}
-                    >
-                      {view.icon}
-                      {view.label}
-                      <Check
-                        className={cn(
-                          "ml-auto h-4 w-4",
-                          value === view.value ? "opacity-100" : "opacity-0",
-                        )}
-                      />
-                    </CommandItem>
-                  ))}
-                </CommandGroup>
-              </CommandList>
-            </Command>
-          </PopoverContent>
-        </div>
-      </Popover>
-    </div>
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-full justify-between opacity-50 hover:opacity-100 transition-opacity"
+        >
+          <div className="flex items-center">
+            {selectedView?.icon}
+            {selectedView?.label || "Select view..."}
+          </div>
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-full p-0">
+        <Command className="bg-pink-500">
+          <CommandInput placeholder="Search view..." />
+          <CommandList>
+            <CommandEmpty>No view found.</CommandEmpty>
+            <CommandGroup>
+              {views.map((view) => (
+                <CommandItem
+                  key={view.value}
+                  value={view.value}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue);
+                    navigate(`/${currentValue}`);
+                    setOpen(false);
+                  }}
+                >
+                  {view.icon}
+                  {view.label}
+                  <Check
+                    className={cn(
+                      "ml-auto h-4 w-4",
+                      value === view.value ? "opacity-100" : "opacity-0",
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
   );
 }
