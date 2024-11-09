7ef2e3f2d1a6c5c5bfabf0ba24a142fbb8082140
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-08 18:26:34 -0800

    checkpoint

--- app/lib/generated/actions.ts
+++ app/lib/generated/actions.ts
@@ -1,18 +1,18 @@
 // app/lib/generated/actions.ts
 // This file is auto-generated. Do not edit it manually.
 
-import { master } from "~/actions/master.server";
-import { loadSelectors } from "~/actions/load-selectors.server";
-import { loadCsvData } from "~/actions/load-csv-data.server";
-import { loadActions } from "~/actions/load-actions.server";
-import { getTables } from "~/actions/get-tables.server";
-import { getActionCallTree } from "~/actions/get-action-call-tree.server";
+import { master } from "../actions/master.server";
+import { loadSelectors } from "../actions/load-selectors.server";
+import { loadCsvData } from "../actions/load-csv-data.server";
+import { loadActions } from "../actions/load-actions.server";
+import { getTables } from "../actions/get-tables.server";
+import { getActionCallTree } from "../actions/get-action-call-tree.server";
 
 export const actions = {
-  master: master,
+  "master": master,
   "load-selectors": loadSelectors,
   "load-csv-data": loadCsvData,
   "load-actions": loadActions,
   "get-tables": getTables,
-  "get-action-call-tree": getActionCallTree,
+  "get-action-call-tree": getActionCallTree
 } as const;
