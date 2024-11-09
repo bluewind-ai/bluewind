bfc05b6226f9180efa8c875be01c830f0af0a2bb
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-09 13:30:15 -0800

    checkpoint

--- app/lib/generated/actions.ts
+++ app/lib/generated/actions.ts
@@ -4,19 +4,19 @@
 import { getFunctionCallTree } from "~/actions/get-function-call-tree.server";
 import { getTables } from "~/actions/get-tables.server";
 import { loadActions } from "~/actions/load-actions.server";
+import { loadAppsToDb } from "~/actions/load-apps-to-db.server";
 import { loadCsvData } from "~/actions/load-csv-data.server";
 import { loadFiles } from "~/actions/load-files.server";
 import { loadSelectors } from "~/actions/load-selectors.server";
-import { loadAppsToDB } from "~/actions/load-apps-to-db.server";
 import { master } from "~/actions/master.server";
 
 export const actions = {
   "get-function-call-tree": getFunctionCallTree,
   "get-tables": getTables,
   "load-actions": loadActions,
+  "load-apps-to-db": loadAppsToDb,
   "load-csv-data": loadCsvData,
   "load-files": loadFiles,
   "load-selectors": loadSelectors,
-  loadAppsToDB: loadAppsToDB,
-  master: master,
+  "master": master
 } as const;
