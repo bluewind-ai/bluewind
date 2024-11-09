01deb7d96727dfacec77bce521a32962389c71a7
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-08 18:20:31 -0800

    checkpoint

--- app/lib/generated/actions.ts
+++ app/lib/generated/actions.ts
@@ -2,23 +2,17 @@
 // This file is auto-generated. Do not edit it manually.
 
 import { master } from "~/actions/master.server";
-import { loadAppsToDB } from "~/actions/loadAppsToDB.server";
 import { loadSelectors } from "~/actions/load-selectors.server";
 import { loadCsvData } from "~/actions/load-csv-data.server";
 import { loadActions } from "~/actions/load-actions.server";
 import { getTables } from "~/actions/get-tables.server";
 import { getActionCallTree } from "~/actions/get-action-call-tree.server";
-import { generateApps } from "~/actions/generateApps.server";
-import { executeGenerateApps } from "~/actions/executeGenerateApps.server";
 
 export const actions = {
   master: master,
-  loadAppsToDB: loadAppsToDB,
   "load-selectors": loadSelectors,
   "load-csv-data": loadCsvData,
   "load-actions": loadActions,
   "get-tables": getTables,
   "get-action-call-tree": getActionCallTree,
-  generateApps: generateApps,
-  executeGenerateApps: executeGenerateApps,
 } as const;
