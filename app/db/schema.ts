c210bb40551e9c74b27b5d096385d9e3bd2b2fb7
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-09 10:28:02 -0800

    fixes

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,22 +1,33 @@
 // app/db/schema.ts
 
-import { pgTable, serial, integer, varchar, text, timestamp, jsonb, pgEnum } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  serial,
+  integer,
+  varchar,
+  text,
+  timestamp,
+  jsonb,
+  pgEnum,
+} from "drizzle-orm/pg-core";
 import { type AnyPgColumn } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 
 export enum ActionType {
-  SYSTEM = 'system',
-  USER = 'user',
-  WORKFLOW = 'workflow',
+  SYSTEM = "system",
+  USER = "user",
+  WORKFLOW = "workflow",
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function enumToPgEnum<T extends Record<string, any>>(
   myEnum: T,
 ): [T[keyof T], ...T[keyof T][]] {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return Object.values(myEnum).map((value: any) => `${value}`) as any;
 }
 
-export const actionTypeEnum = pgEnum('action_type', enumToPgEnum(ActionType));
+export const actionTypeEnum = pgEnum("action_type", enumToPgEnum(ActionType));
 
 export const apps = pgTable("apps", {
   id: serial("id").primaryKey(),
