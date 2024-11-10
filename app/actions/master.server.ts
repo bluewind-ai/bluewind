2e4ed0df32e6cced99153a2226f9d7d6b6f9accd
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-09 17:13:55 -0800

    checkpoint

--- app/actions/master.server.ts
+++ app/actions/master.server.ts
@@ -1,9 +1,8 @@
 // app/actions/master.server.ts
 
 import { createAction } from "~/lib/action-builder.server";
-import { loadCsvData } from "./load-csv-data.server";
+import { loadFiles } from "./load-files.server";
 
 export const master = createAction("master", async () => {
-  const csvData = await loadCsvData();
-  return csvData;
+  await loadFiles();
 });
