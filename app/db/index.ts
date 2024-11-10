b142bf0bcfb67b2748ec4a3d434d909a0ad6fbf3
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-09 16:50:18 -0800

    fixes

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,10 +1,7 @@
 // app/db/index.ts
 
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import * as schema from "./schema";
+import { createDbClient } from "./db-client";
 
 const connectionString = `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
-const client = postgres(connectionString);
-export const db = drizzle(client, { schema });
+export const db = createDbClient(connectionString);
