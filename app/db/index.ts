ce381b8d628a58057bcc6bddbc07c63c548820d5
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-09 18:44:03 -0800

    fixes

--- app/db/index.ts
+++ app/db/index.ts
@@ -11,8 +11,6 @@ const client = postgres(connectionString);
 const baseDb = drizzle(client, { schema });
 
 function createProxy() {
-  let currentTable: PgTable<any> | null = null;
-
   const handler = {
     get(target: PostgresJsDatabase<typeof schema>, prop: string | symbol) {
       const original = target[prop as keyof typeof target];
@@ -25,7 +23,6 @@ function createProxy() {
             table: table[Symbol.for("drizzle:Name")],
           });
 
-          currentTable = table;
           const chain = insertFn(table);
 
           console.log("CHAIN METHODS:", chain);
