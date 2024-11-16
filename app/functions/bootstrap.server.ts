aa99781dc8eb99f37cca632b9b4f1327b2e7eacb
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-16 11:31:06 -0800

    bootstrap should truncate first

--- app/functions/bootstrap.server.ts
+++ app/functions/bootstrap.server.ts
@@ -4,12 +4,20 @@ import * as schema from "~/db/schema";
 import { functionCalls, FunctionCallStatus } from "~/db/schema";
 import type { RequestExtensions } from "~/middleware";
 
+import { truncateDb } from "./truncate-db.server";
+
 export async function bootstrap(request: RequestExtensions) {
   const db = request.db;
+
+  // First truncate everything and reseed models
+  await truncateDb(request);
+
+  // Now we can be sure we have a request from the seedModels
   const foundRequest = await db.query.requests.findFirst();
   if (!foundRequest) {
     throw new Error("No request found");
   }
+
   const [masterAction] = await db
     .insert(schema.serverFunctions)
     .values({
@@ -18,6 +26,7 @@ export async function bootstrap(request: RequestExtensions) {
       type: schema.ServerFunctionType.SYSTEM,
     })
     .returning();
+
   await db
     .insert(functionCalls)
     .values({
@@ -26,5 +35,6 @@ export async function bootstrap(request: RequestExtensions) {
       status: FunctionCallStatus.READY_FOR_APPROVAL,
     })
     .returning();
+
   await new Promise((resolve) => setTimeout(resolve, 1));
 }
