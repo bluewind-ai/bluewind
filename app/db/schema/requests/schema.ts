a3137962adae89ad8f5613ecd664df46830a293a
Merwane Hamadi <merwanehamadi@gmail.com>
2024-11-28 10:55:17 -0800

    add created at to schema
    Signed-off-by: Merwane Hamadi <merwanehamadi@gmail.com>

--- app/db/schema/requests/schema.ts
+++ app/db/schema/requests/schema.ts
@@ -1,25 +1,27 @@
 // app/db/schema/requests/schema.ts
 
 import { relations } from "drizzle-orm";
-import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 import { z } from "zod";
 
 import { serverFunctions } from "../server-functions/schema";
 
 export const requests = pgTable("requests", {
   id: serial("id").primaryKey(),
-  parentId: integer("parent_id"), // Changed from requestId
+  parentId: integer("parent_id"),
   pathname: text("pathname").notNull(),
   createdLocation: text("created_location").notNull(),
   response: text("response"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
 });
 
 export const RequestSchema = z.object({
   id: z.number(),
-  parentId: z.number().nullable(), // Changed from requestId
+  parentId: z.number().nullable(),
   pathname: z.string(),
   createdLocation: z.string(),
   response: z.string().nullable(),
+  createdAt: z.date(),
 });
 
 export type CreateRequest = z.infer<typeof RequestSchema>;
---
+++ drizzle/migrations/0029_swift_kid_colt.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "requests" ADD COLUMN "created_at" timestamp DEFAULT now() NOT NULL;
